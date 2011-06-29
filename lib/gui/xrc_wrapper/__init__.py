93906941e209624d1cd8f5f755ebaad516ad5691
Julien Lamy <lamy@unistra.fr>
2011-06-29 18:24:23 +0200

    lib/gui/xrc_wrapper : initial import

---
+++ lib/gui/xrc_wrapper/__init__.py
@@ -0,0 +1,13 @@
+##########################################################################
+# MediPy - Copyright (C) Universite de Strasbourg, 2011             
+# Distributed under the terms of the CeCILL-B license, as published by 
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
+# for details.                                                      
+##########################################################################
+
+from medipy.gui.xrc_wrapper.dialog import Dialog
+from medipy.gui.xrc_wrapper.evt_handler import EvtHandler
+from medipy.gui.xrc_wrapper.frame import Frame
+from medipy.gui.xrc_wrapper.top_level_window import TopLevelWindow
+from medipy.gui.xrc_wrapper.window import Window
