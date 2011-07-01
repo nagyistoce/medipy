cc26ee9668c3a9926803ab388597d93439f51924
Julien Lamy <lamy@unistra.fr>
2011-07-01 11:06:03 +0200

    apps : initial import of MediPy app

---
+++ apps/medipy/medipy_app.py
@@ -0,0 +1,438 @@
+##########################################################################
+# MediPy - Copyright (C) Universite de Strasbourg, 2011             
+# Distributed under the terms of the CeCILL-B license, as published by 
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
+# for details.                                                      
+##########################################################################
+
+from vtk import (vtkDataReader, vtkPolyDataReader, vtkPolyDataWriter,
+    vtkRectilinearGridReader, vtkStructuredGridReader, 
+    vtkUnstructuredGridReader, vtkVRMLImporter,)
+
+import wx
+_ = wx.GetTranslation
+
+import medipy.base
+import medipy.gui.image
+import medipy.gui.dicom
+import medipy.gui.dicom.reconstruction
+
+from medipy.gui.annotations.annotations_dialog import AnnotationsDialog
+from medipy.gui.base import Application
+from medipy.gui.image.cine_dialog import CineDialog
+from medipy.gui.image.layers_dialog import LayersDialog
+from medipy.gui.image.tools import MouseTool
+
+from medipy.base import ObservableList, Object3D
+
+from main_frame import MainFrame
+
+class MediPyApp(Application) :
+    
+    def __init__(self, functions_path, *args, **kwargs):
+        
+        # Public interface
+        self.images = ObservableList()
+        self.gui_images = ObservableList()
+        self.viewer_3ds = ObservableList()
+        
+        # Properties
+        self._synchronize_images = {}
+        
+        # Private interface
+        self._frame = None
+        self._functions_path = functions_path
+        self._active_image_index = None
+        self._full_screen = False
+        self._cine_dialog = None
+        self._layers_dialog = None
+        self._annotations_dialog = None
+        
+        super(MediPyApp, self).__init__(*args, **kwargs)
+    
+    ####################
+    # Public interface #
+    ####################
+        
+    def insert_image(self, index, image, layers=None):  
+        """ Image and layers must be three dimensional
+        """
+        
+        layers = layers or []
+        
+        self._frame.Freeze()
+        
+        self.images.insert(index, image)
+        
+        if len(image.shape) == 2 or len(image.shape) == 3 and image.shape[0] == 1 :
+            mode = "axial"
+        else : 
+            mode = "multiplanar"
+        
+        gui_image = medipy.gui.image.Image(
+            self._frame.images_panel, mode, 
+            layers = [{"image" : image}] + [{"image" : l} for l in layers],
+            annotations = image.annotations,
+        )
+        gui_image.reset_view()
+        gui_image.Bind(wx.EVT_LEFT_DOWN, self.OnImageClicked)
+        gui_image.Bind(wx.EVT_MIDDLE_DOWN, self.OnImageClicked)
+        gui_image.Bind(wx.EVT_RIGHT_DOWN, self.OnImageClicked)
+        gui_image.Bind(wx.EVT_MOUSEWHEEL, self.OnImageClicked)
+        
+        if self.gui_images :
+            reference_image = self.gui_images[0]
+            if self._synchronize_images["cursor_position"] :
+                gui_image.cursor_physical_position = reference_image.cursor_physical_position
+            if self._synchronize_images["center"] :
+                gui_image.center_physical_position = reference_image.center_physical_position
+            if self._synchronize_images["zoom"] :
+                gui_image.zoom = reference_image.zoom
+            if self._synchronize_images["display_range"] :
+                display_range = reference_image.get_layer_colormap(0).display_range
+                gui_image.get_layer_colormap(0).display_range = display_range
+        
+        gui_image.add_observer("cursor_position", self._on_cursor_position)
+        gui_image.add_observer("center", self._on_center)
+        gui_image.add_observer("colormap_display_range", self._on_colormap_display_range)
+        
+#        gui_image.set_mouse_button_tool("Right", ContextMenuImageTool, self._frame, "Right")
+        
+        self.gui_images.insert(index, gui_image)
+        
+        self.active_image = gui_image
+        
+        self._frame.insert_image(index, gui_image)
+        
+        # Set which attributes are synchronized, set them to another image if
+        # they are to be synchronized
+        gui_image.synchronize_on_event["zoom"] = self._synchronize_images["zoom"]
+            
+        # Set children for synchronization :
+        for image_index, gui_image in enumerate(self.gui_images) :
+            next_image = self.gui_images[(image_index+1)%len(self.gui_images)]
+            gui_image.delete_all_children()
+            gui_image.append_child(next_image)
+        
+        self._frame.Thaw()
+    
+    def append_image(self, image, layers = None):
+        return self.insert_image(len(self.gui_images), image, layers)
+    
+    def insert_viewer_3d(self, index, viewer_3d):
+        self.viewer_3ds.insert(index, viewer_3d)
+        viewer_3d.add_observer("close", self._on_viewer_3d_close)
+    
+    def append_viewer_3d(self, viewer_3d):
+        return self.insert_viewer_3d(len(self.viewer_3ds), viewer_3d)
+    
+    def insert_layer(self, image_index, layer_index, layer_data):
+        self.gui_images[image_index].insert_layer(self, layer_index, 
+                                                  **layer_data)
+    
+    def append_layer(self, image_index, layer_data):
+        self.insert_layer(image_index, 
+                          self.gui_images[image_index].number_of_layers, 
+                          layer_data)
+    
+    def set_cine_dialog_visibility(self, visibility) :
+        if visibility :
+            if self._active_image_index is not None :
+                image = self.gui_images[self._active_image_index]
+                if self._cine_dialog is None :
+                    self._cine_dialog = CineDialog(self._frame, wx.ID_ANY)
+                self._cine_dialog.image = image
+                self._cine_dialog.Show()
+            elif self._cine_dialog is not None :
+                self._cine_dialog.Hide()
+        else :
+            self._cine_dialog.Hide()
+    
+    def toggle_synchronize_images(self, attribute) :
+        self.set_synchronize_images(attribute,
+                                    not self.get_synchronize_images(attribute))
+    
+    def toggle_full_screen(self):
+        self._full_screen = not self._full_screen
+        
+        if self._full_screen : 
+            self._frame.full_screen(self.active_image)
+        else :
+            self._frame.full_screen(None)      
+    
+    def close_image(self, gui_image):
+        
+        dialogs = ["cine", "layers", "annotations"]
+        for name in dialogs :
+            dialog = getattr(self, "_%s_dialog"%name)
+            if dialog is not None :
+                dialog.Destroy()
+                setattr(self, "_%s_dialog"%name, None)
+        
+        gui_image_index = self.gui_images.index(gui_image)
+        image_index = self.images.index(gui_image.get_layer_image(0))
+        
+        self._frame.delete_image(gui_image)
+        gui_image.Close()
+        del self.gui_images[gui_image_index]
+        
+        if image_index is not None :
+            del self.images[image_index]
+        
+        if len(self.images) > 0 and self._active_image_index >= len(self.images) : 
+            self._set_active_image(self.gui_images[-1])
+        elif len(self.images) > 0 :
+            self._set_active_image(self.gui_images[self._active_image_index])
+        
+        # Set children for synchronization :
+        for image_index, gui_image in enumerate(self.gui_images) :
+            next_image = self.gui_images[(image_index+1)%len(self.gui_images)]
+            gui_image.delete_all_children()
+            gui_image.append_child(next_image)
+    
+    def close_all_images(self) :
+        while len(self.gui_images)>0 : 
+            self.close_image(self.gui_images[0])
+    
+    def set_layers_dialog_visibility(self, visibility):
+        if visibility :
+            if self._active_image_index is not None :
+                image = self.gui_images[self._active_image_index]
+                if self._layers_dialog is None :
+                    self._layers_dialog = LayersDialog(self._frame, wx.ID_ANY)
+                self._layers_dialog.image = image
+                self._layers_dialog.Show()
+            elif self._layers_dialog is not None :
+                self._layers_dialog.Hide()
+        else :
+            self._layers_dialog.Hide()
+    
+    def set_annotations_dialog_visibility(self, visibility):
+        if visibility :
+            if self._active_image_index is not None :
+                image = self.gui_images[self._active_image_index]
+                if self._annotations_dialog is None :
+                    self._annotations_dialog = AnnotationsDialog(self._frame)
+                self._annotations_dialog.image = image
+                self._annotations_dialog.Show()
+            elif self._annotations_dialog is not None :
+                self._annotations_dialog.Hide()
+        else :
+            self._annotations_dialog.Hide()
+    
+    def reset_view(self):
+        self.active_image.reset_view()
+        self.active_image.render()
+    
+    def current_image_screenshot(self):
+        # Set image to inactive, avoid green border
+        self.active_image.active = False
+        self.active_image.Refresh()
+        self.active_image.Update()
+        
+        image = self._image_from_window(self.active_image)
+        
+        # Reset the active status
+        self.active_image.active = True
+        self.active_image.Refresh()
+        self.active_image.Update()
+        
+        return image
+    
+    def all_images_screenshot(self):
+        return self._image_from_window(self._frame.images_panel)
+    
+    def whole_window_screenshot(self):
+        return self._image_from_window(self._frame)
+    
+    def execute_script(self, filename):
+        execfile(filename, globals(), locals())
+    
+    def quit(self):
+        self.close_all_images()
+        for viewer in self.viewer_3ds :
+            viewer.Close()
+    
+    def load_object_3d(self, path, viewer_3d):
+        generic_reader = vtkDataReader()
+        generic_reader.SetFileName(path)
+        
+        if generic_reader.OpenVTKFile() and generic_reader.ReadHeader() :
+            if generic_reader.IsFileStructuredPoints() :
+                raise Exception("Cannot read VTK structured points")
+            elif generic_reader.IsFilePolyData() :
+                reader = vtkPolyDataReader()
+                reader.SetFileName(path)
+                object_3d = Object3D(reader.GetOutput(), path)
+            elif generic_reader.IsFileStructuredGrid() :
+                reader = vtkStructuredGridReader()
+                reader.SetFileName(path)
+                object_3d = Object3D(reader.GetOutput(), path)
+            elif generic_reader.IsFileUnstructuredGrid() :
+                reader = vtkUnstructuredGridReader()
+                reader.SetFileName(path)
+                object_3d = Object3D(reader.GetOutput(), path)
+            elif generic_reader.IsFileRectilinearGrid() :
+                reader = vtkRectilinearGridReader()
+                reader.SetFileName(path)
+                object_3d = Object3D(reader.GetOutput(), path)
+            else : 
+                raise Exception("Cannot read VTK file containing type %i"%generic_reader.GetFileType())
+            viewer_3d.objects_3d.append(object_3d)
+        else :
+            importer = vtkVRMLImporter()
+            importer.SetFileName(path)
+            importer.Update()
+            actors = importer.GetRenderer().GetActors()
+            number_of_actors = actors.GetNumberOfItems()
+            actors.InitTraversal()
+            for i in range(number_of_actors) :
+                actor = actors.GetNextItem()
+                object_3d = Object3D(path + ", %i"%i)
+                object_3d.actor.SetProperty(actor.GetProperty())
+                object_3d.actor.SetMapper(actor.GetMapper())
+                viewer_3d.objects_3d.append(object_3d)
+#        else :
+#            raise Exception("Cannot load file %s of unknown type"%path)
+    
+    def save_object_3d(self, object_3d, path) :
+        writer = vtkPolyDataWriter()
+        writer.SetFileName(path)
+        writer.SetFileTypeToBinary()
+        writer.SetHeader(object_3d.name)
+        writer.SetInput(object_3d.dataset)
+        writer.Update()
+    
+    def get_synchronize_images(self, attribute):
+        return self._synchronize_images[attribute]
+    
+    def set_synchronize_images(self, attribute, value) :
+        self._synchronize_images[attribute] = value
+        wx.ConfigBase_Get().WriteBool("SynchronizeImages_%s"%attribute, value)
+        wx.ConfigBase_Get().Flush()
+        
+        if attribute == "zoom" :
+            for image in self.gui_images :
+                if hasattr(image, attribute) :
+                    image.synchronize_on_event[attribute] = value
+    
+    ##############
+    # PROPERTIES #
+    ##############
+
+    def _set_slices(self, slices):
+        if slices != self.active_image.slice_mode :
+            self.active_image.slice_mode = slices
+            self.active_image.render()
+        self._frame.slices = slices
+    
+    slices = property(lambda self:self.active_image.slices, _set_slices)
+    
+    def _set_active_image(self, gui_image):
+        try :
+            self._active_image_index = self.gui_images.index(gui_image)
+        except ValueError :
+            # Image is not in list, do nothing
+            pass
+        for other_image in self.gui_images :
+            if other_image == gui_image :
+                other_image.SetBackgroundColour(wx.GREEN)
+            else :
+                other_image.SetBackgroundColour(wx.BLACK)
+        self._frame.slices = gui_image.slice_mode
+        if self._cine_dialog :
+            self._cine_dialog.image = gui_image
+        if self._layers_dialog :
+            self._layers_dialog.image = gui_image
+        if self._annotations_dialog is not None :
+            self._annotations_dialog.image = gui_image
+            self._annotations_dialog.annotations = gui_image.annotations
+    
+    def _get_active_image(self):
+        if self._active_image_index != -1 :
+            return self.gui_images[self._active_image_index]
+        else :
+            return None
+    
+    active_image = property(_get_active_image, _set_active_image)
+    
+    ##################
+    # Event handlers #
+    ##################
+    def OnInit(self) :
+        self.SetAppName(_("MediPy"))
+        
+        for attribute in ["cursor_position", "center", "zoom", "display_range"] :
+            config_entry = "SynchronizeImages_%s"%attribute
+            value = wx.ConfigBase_Get().ReadBool(config_entry) or False
+            self.set_synchronize_images(attribute, value)
+        
+        self._frame = MainFrame(parent=None, id=-1, 
+                                  title=_("MediPy"), size=(1000,800),
+                                  functions_path = self._functions_path)
+        self._frame.application = self
+        self._frame.Show()
+        self.SetTopWindow(self._frame)
+        
+        return True
+    
+    def OnImageClicked(self, event):
+        self.active_image = event.GetEventObject()
+    
+    def _on_cursor_position(self, event) :
+        
+        if not self._synchronize_images.get("cursor_position", False) :
+            return
+        
+        for gui_image in self.gui_images :
+            if gui_image != event.object :
+                gui_image.remove_observer("cursor_position", self._on_cursor_position)
+                gui_image.cursor_physical_position = event.object.cursor_physical_position
+                gui_image.add_observer("cursor_position", self._on_cursor_position)
+        
+    def _on_center(self, event) :
+        
+        if not self._synchronize_images.get("center", False) :
+            return
+        
+        for gui_image in self.gui_images :
+            if gui_image != event.object :
+                gui_image.remove_observer("center", self._on_center)
+                gui_image.center_physical_position = event.object.center_physical_position
+                gui_image.render()
+                gui_image.add_observer("center", self._on_center)
+    
+    def _on_colormap_display_range(self, event):
+        
+        if not self._synchronize_images.get("display_range", False) :
+            return
+        
+        for gui_image in self.gui_images :
+            if gui_image != event.object :
+                gui_image.remove_observer("colormap_display_range", self._on_colormap_display_range)
+                source_colormap = event.object.get_layer_colormap(event.layer_index)
+                destination_colormap = gui_image.get_layer_colormap(event.layer_index)
+                destination_colormap.display_range = source_colormap.display_range
+                gui_image.render()
+                gui_image.add_observer("colormap_display_range", self._on_colormap_display_range)
+    
+    def _on_viewer_3d_close(self, event):
+        viewer = event.object
+        index = self.viewer_3ds.index(viewer)
+        del self.viewer_3ds[index]
+    
+    #####################
+    # Private interface #
+    #####################
+    
+    def _image_from_window(self, window):
+        self._frame.Refresh()
+        self._frame.Update()
+        screen = wx.ClientDC(window)
+        size = window.GetSize()
+        image = wx.EmptyBitmap(size[0], size[1], -1)
+        memory = wx.MemoryDC()
+        memory.SelectObject(image)
+        memory.Blit(0, 0, size.GetWidth(), size.GetHeight(), screen, 0, 0)
+        return image
---
+++ apps/medipy/menu_builder/from_file.py
@@ -0,0 +1,174 @@
+##########################################################################
+# MediPy - Copyright (C) Universite de Strasbourg, 2011             
+# Distributed under the terms of the CeCILL-B license, as published by 
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
+# for details.                                                      
+##########################################################################
+
+import logging
+import re
+
+def get_level(line, indent) :
+    """ Return the level of indentation of a given line
+    """
+    
+    level = 0
+    while line.startswith(indent) :
+        line = line[len(indent):]
+        level += 1
+    if re.match(r"^(\s+)", line) :
+        raise Exception("Syntax Error")
+    return level
+
+def build_hierarchy(lines, root, top_level, first_children, first_siblings, path=[]) :
+    """ Build a hierarchy of lines rooted at given line. Each item is of the
+        form (name, display_name, sub_items)
+    """
+    
+    sub_items = []
+    
+    line = lines[root].strip()
+    leaf, display_name = line.split(" ", 1) if " " in line else (line, None)
+    
+    if root in first_children :
+        child = first_children[root]
+        sub_item = build_hierarchy(lines, child, top_level, 
+                                   first_children, first_siblings,
+                                   path = path+[leaf])
+        sub_items.append(sub_item)
+        
+        while child in first_siblings :
+            child = first_siblings[child]
+            sub_item = build_hierarchy(lines, child, top_level, 
+                                       first_children, first_siblings,
+                                       path = path+[leaf])
+            sub_items.append(sub_item)
+    
+    if not display_name :
+        display_name = display_name or leaf.split(".")[-1]
+        display_name = display_name.split("_")
+        display_name[0] = display_name[0].capitalize()
+        display_name = " ".join(display_name)
+    
+    if not sub_items :
+        module_name = (".".join(["medipy", "components"]+path)
+                       if not leaf.startswith("medipy.components.") 
+                       else ".".join(leaf.split(".")[:-1]))
+        function_name = (leaf if not leaf.startswith("medipy.components.") 
+                         else leaf.split(".")[-1])
+        
+        namespace = {}
+        try :
+            exec "from %s import %s"%(module_name, function_name) in namespace
+        except ImportError :
+            logging.error(
+                "No function named %s in module %s", function_name, module_name)
+            function = None
+        else :
+            function = namespace[function_name]
+    else : 
+        function = None
+    
+    return (display_name, function, sub_items)
+
+def build_menu(filename):
+    """ Return a hierarchical representation of a menu described in the file.
+        Such a file will look like the following :  
+        arithmetic
+            addition
+            soustraction
+            division
+        medimax3
+            recalage Registration
+                recalage_lineaire_multi_IM
+                recalage_Bspline_topo
+                ApplyTransfo3d_GUI Apply 3D Transform
+        segmentation
+            bet BET
+    
+        Each line has the following syntax : <indentation><name> [displayed name]
+    """
+    
+    # Read all lines
+    try :
+        file = open(filename)
+    except IOError, e :
+        logging.error("Cannot open file %s : %s", filename, e)
+        return []
+    try :
+        lines = file.readlines()
+        # Remove comments
+        lines = [x.split("#")[0] for x in lines]
+        # Remove trailing spaces and end of lines
+        lines = [x.rstrip() for x in lines]
+        # Remove blank lines
+        lines = [x for x in lines if x]
+    except IOError :
+        file.close()
+        return []
+    file.close()
+    
+    # Determine indentation used in the file : use the indentation of the first
+    # line
+    indent = ""
+    for line in lines :
+        match = re.match(r"^(\s+)", line)
+        if match :
+            indent = match.group(0)
+            break
+    
+    # Build tree hierarchy : top-level items, first child and first sibling of
+    # each item
+    top_level = []
+    first_children = {}
+    first_siblings = {}
+    
+    for index in range(len(lines)-1) :
+        
+        line = lines[index]
+        
+        level = get_level(line, indent)
+        
+        if level == 0 :
+            top_level.append(index)
+        
+        next_level = get_level(lines[index+1], indent)
+        if next_level == level+1 :
+            first_children[index] = index+1 
+        
+        # Look for siblings
+        for other_line_index in range(index+1, len(lines)) :
+            other_line = lines[other_line_index]
+            
+            other_level = get_level(other_line, indent)
+            if level == other_level :
+                # Found first sibling
+                first_siblings[index] = other_line_index
+                break
+            elif level > other_level :
+                # Went too far, in another menu
+                break
+    
+    return [build_hierarchy(lines, x, top_level, first_children, first_siblings)
+            for x in top_level]
+
+if __name__ == "__main__" :
+    import wx
+    from medipy.base import find_resource
+    from medipy.gui.menu_builder import fill_treectrl
+    
+    menu = build_menu(find_resource("gui/full.menu"))
+    
+    app = wx.PySimpleApp()
+    frame = wx.Frame(None)
+    sizer = wx.BoxSizer()
+    frame.SetSizer(sizer)
+    
+    treectrl = wx.TreeCtrl(frame, 
+        style=wx.TR_DEFAULT_STYLE|wx.TR_HAS_BUTTONS|wx.TR_SINGLE|wx.TR_HIDE_ROOT)
+    fill_treectrl(menu, treectrl)
+    sizer.Add(treectrl, 1, wx.EXPAND)
+    
+    frame.Show()
+    app.MainLoop()
