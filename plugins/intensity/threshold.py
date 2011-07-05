1e546f1d30325147274f9481f64a202fec6bb642
Julien Lamy <lamy@unistra.fr>
2011-07-05 14:47:53 +0200

    plugins/intensity : add threshold function

--- plugins/intensity/threshold.py
+++ plugins/intensity/threshold.py
@@ -36,3 +36,28 @@ def binary_threshold(input=None, min=None, max=None, inside=None, outside=None) 
     itk_output = threshold_filter[0]
     
     return medipy.itk.itk_image_to_medipy_image(itk_output, None, True)
+
+def threshold(input=None, min=None, max=None, outside=None) :
+    """ Return a thresholded version of the input image.
+    
+        <gui> 
+            <item name="input" type="Image" label="Image"/>
+            <item name="min" type="Float" label="Minimum value"
+                tooltip="Minimum value in threshold (included)"/>
+            <item name="max" type="Float" label="Maximum value"
+                tooltip="Maximum value in threshold (included)"/>
+            <item name="outside" type="Float" initializer="0" label="Outside value"
+                tooltip="Value taken by pixels outside the image"/>
+            <item name="output" type="Image" initializer="output=True"
+                role="return" label="Output image"/>
+        </gui>
+    """
+    itk_input = medipy.itk.medipy_image_to_itk_image(input, False)
+    
+    threshold_filter = itk.ThresholdImageFilter[itk_input].New(
+        Lower=min, Upper=max, OutsideValue=outside)
+
+    threshold_filter(itk_input)
+    itk_output = threshold_filter[0]
+    
+    return medipy.itk.itk_image_to_medipy_image(itk_output, None, True)
