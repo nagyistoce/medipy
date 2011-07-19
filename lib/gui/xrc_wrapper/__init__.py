5a1fcbc126dd6a695ef745b33e5c27faa7edd4c9
Julien Lamy <lamy@unistra.fr>
2011-07-19 14:26:56 +0200

    lib/gui/xrc_wrapper : add handlers for DicomExplorer and String

--- lib/gui/xrc_wrapper/__init__.py
+++ lib/gui/xrc_wrapper/__init__.py
@@ -15,4 +15,6 @@ from medipy.gui.xrc_wrapper.window import Window
 from medipy.gui.xrc_wrapper.scrolled_panel_xml_handler import ScrolledPanelXMLHandler
 
 from medipy.gui.xrc_wrapper.control_image_xml_handler import ControlImageXMLHandler
+from medipy.gui.xrc_wrapper.dicom_explorer_xml_handler import DICOMExplorerXMLHandler
 from medipy.gui.xrc_wrapper.viewer_3d_xml_handler import Viewer3DXMLHandler
+from medipy.gui.xrc_wrapper.string_xml_handler import StringXMLHandler
