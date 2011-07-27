b998414c7f75c4d11b293d08c3c9399a2fcc24f6
Julien Lamy <lamy@unistra.fr>
2011-07-27 10:40:21 +0200

    lib/itk : add wrap_itk_loader

--- lib/itk/__init__.py
+++ lib/itk/__init__.py
@@ -10,30 +10,7 @@ from numpy_bridge import (array_to_itk_image, array_to_itk_vector_image,
                           itk_image_to_array, itk_vector_image_to_array, 
                           medipy_image_to_itk_image, itk_image_to_medipy_image, 
                           itk_to_dtype, dtype_to_itk)
-                         
-
-def load_wrapitk_module(path, name):
-    """ Load a WrapITK module, and add its symbols to the itk module """
-    import sys
-    
-    import itkBase
-    import itk
-    
-    sys.path.append(path)
-    
-    data = {}
-    
-    import MediPyBridgeConfig
-    data = MediPyBridgeConfig.__dict__
-    
-    itkBase.module_data[name] = data
-
-    namespace = {}
-    itkBase.LoadModule(name, namespace)
-
-    for k, v in namespace.items():
-        if not hasattr(itk, k) :
-            setattr(itk, k, v)
+from loader import load_wrapitk_module
 
 import os.path
 load_wrapitk_module(os.path.dirname(__file__), "MediPyBridge")
--- plugins/segmentation/__init__.py
+++ plugins/segmentation/__init__.py
@@ -1,28 +1,7 @@
 from api import *
 
-def load_wrapitk_module(path, name):
-    """ Load a WrapITK module, and add its symbols to the itk module """
-    
-    import sys
-    
-    import itkBase
-    import itk
-    
-    sys.path.append(path)
-    
-    data = {}
-    
-    import BETImageFilterConfig
-    data = BETImageFilterConfig.__dict__
-    
-    itkBase.module_data[name] = data
-
-    namespace = {}
-    itkBase.LoadModule(name, namespace)
-
-    for k, v in namespace.items():
-        if not hasattr(itk, k) :
-            setattr(itk, k, v)
-
 import os.path
-load_wrapitk_module(os.path.dirname(__file__), "BETImageFilter")
+import medipy.itk
+
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "BETImageFilter")
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "StatisticalChangeDetectionImageFilters")
