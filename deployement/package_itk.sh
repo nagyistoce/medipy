9bd3522434ae698377910c32151a2de5c67b4271
Julien Lamy <lamy@unistra.fr>
2011-09-16 17:14:11 +0200

    deployement : add missing license to packaging script

--- deployement/package_itk.sh
+++ deployement/package_itk.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+##########################################################################
+# MediPy - Copyright (C) Universite de Strasbourg, 2011             
+# Distributed under the terms of the CeCILL-B license, as published by 
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
+# for details.                                                      
+##########################################################################
+
 MAINTAINER=${MAINTAINER?"environment variable must be defined (e.g. 'John Doe <john@example.com>'"}
 VENDOR=`lsb_release -is`
 RELEASE=`lsb_release -rs`
