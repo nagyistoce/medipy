993993fe1547bbb82b333c7c592b85941e1d82a7
Julien Lamy <lamy@unistra.fr>
2011-09-21 17:29:10 +0200

    plugins/segmentation : add MaskWithValueImageFilter

--- plugins/logic/itkMaskWithValueImageFilter.h
+++ plugins/logic/itkMaskWithValueImageFilter.h
@@ -6,10 +6,10 @@
  * for details.
  ************************************************************************/
 
-#ifndef medipy_components_segmentation_itkmaskwithvalueimagefilter_h
-#define medipy_components_segmentation_itkmaskwithvalueimagefilter_h
+#ifndef b0f7acb3_a658_426b_9de9_f752e64f9468
+#define b0f7acb3_a658_426b_9de9_f752e64f9468
 
-#include <itkBinaryFunctorImageFilter.h>
+#include <itkImageToImageFilter.h>
 #include <itkNumericTraits.h>
 
 
@@ -30,168 +30,64 @@ namespace itk
  * The pixel from the input 1 is cast to the pixel type of the output image.
  *
  * Note that the input and the mask images must be of the same size.
- *
- * \warning Any pixel value other than 0 will not be masked out.
- *
- * \sa MaskNegatedImageFilter
- * \ingroup IntensityImageFilters  Multithreaded
  */
-namespace Functor {
-
-template<typename TInput, typename TMask, typename TOutput = TInput>
-class MaskWithValue
-{
-public:
-    MaskWithValue()
-    : m_BackgroundValue(NumericTraits<TMask>::Zero),
-      m_OutsideValue(NumericTraits<TOutput>::Zero)
-    {
-        // Nothing else
-    }
-
-    ~MaskWithValue()
-    {
-        // Nothing else
-    }
-
-    bool operator!=(MaskWithValue const & ) const
-    {
-        return false;
-    }
-
-    bool operator==(MaskWithValue const & other) const
-    {
-        return !(*this != other);
-    }
-
-    inline TOutput operator()(TInput const & A, TMask const & B) const
-    {
-        if (B != this->m_BackgroundValue)
-        {
-            return static_cast<TOutput>(A);
-        }
-        else
-        {
-            return m_OutsideValue;
-        }
-    }
-
-    /** Method to explicitly set the background value of the mask */
-    void SetBackgroundValue(TMask const & backgroundValue)
-    {
-        m_BackgroundValue = backgroundValue;
-    }
-
-    /** Method to get the outside value of the mask */
-    const TMask & GetBackgroundValue() const
-    {
-        return m_BackgroundValue;
-    }
-
-    /** Method to explicitly set the outside value of the mask */
-    void SetOutsideValue(TOutput const & outsideValue)
-    {
-        m_OutsideValue = outsideValue;
-    }
-
-    /** Method to get the outside value of the mask */
-    const TOutput & GetOutsideValue() const
-    {
-        return m_OutsideValue;
-    }
-
-private:
-    TMask m_BackgroundValue;
-    TOutput m_OutsideValue;
-}; // class MaskWithValue
-
-} // namespace Functor
-
 template <typename TInputImage,
           typename TMaskImage,
           typename TOutputImage=TInputImage>
 class MaskWithValueImageFilter :
-    public BinaryFunctorImageFilter<
-         TInputImage,TMaskImage,TOutputImage,
-         Functor::MaskWithValue<typename TInputImage::PixelType,
-                                typename TMaskImage::PixelType,
-                                typename TOutputImage::PixelType> >
+    public ImageToImageFilter<TInputImage,TOutputImage>
 {
 public:
     /** Standard class typedefs. */
     typedef MaskWithValueImageFilter Self;
-    typedef BinaryFunctorImageFilter<
-        TInputImage,TMaskImage,TOutputImage,
-        Functor::MaskWithValue<typename TInputImage::PixelType,
-                               typename TMaskImage::PixelType,
-                               typename TOutputImage::PixelType> > Superclass;
-    typedef SmartPointer<Self> Pointer;
-    typedef SmartPointer<Self const>  ConstPointer;
 
-    typedef typename TOutputImage::PixelType OutputPixelType;
+    typedef ImageToImageFilter<TInputImage,TOutputImage> Superclass;
+    typedef SmartPointer<Self> Pointer;
+    typedef SmartPointer<Self const> ConstPointer;
 
     /** Method for creation through the object factory. */
     itkNewMacro(Self);
 
     /** Runtime information support. */
-    itkTypeMacro(MaskWithValueImageFilter, BinaryFunctorImageFilter);
+    itkTypeMacro(MaskWithValueImageFilter, ImageToImageFilter);
 
-    /** Set the background value of the mask. Defaults to 0 */
-    void SetBackgroundValue(OutputPixelType const & backgroundValue)
-    {
-        if(this->GetBackgroundValue() != backgroundValue)
-        {
-            this->Modified();
-            this->GetFunctor().SetBackgroundValue(backgroundValue);
-        }
-    }
+    /** Convenient typedefs. */
+    typedef typename Superclass::InputImageType InputImageType;
+    typedef typename Superclass::OutputImageType OutputImageType;
+    typedef typename Superclass::InputImagePixelType InputImagePixelType;
+    typedef typename Superclass::OutputImagePixelType OutputImagePixelType;
 
-    OutputPixelType const & GetBackgroundValue() const
-    {
-        return this->GetFunctor().GetBackgroundValue();
-    }
+    typedef TMaskImage MaskImageType;
+    typedef typename TMaskImage::PixelType MaskImagePixelType;
 
-    /** Set the outside value of the mask. Defaults to 0 */
-    void SetOutsideValue(OutputPixelType const & outsideValue)
-    {
-        if( this->GetOutsideValue() != outsideValue)
-        {
-            this->Modified();
-            this->GetFunctor().SetOutsideValue(outsideValue);
-        }
-    }
+    MaskImageType const * GetMask();
+    void SetMask(MaskImageType const * image);
 
-    OutputPixelType const & GetOutsideValue() const
-    {
-        return this->GetFunctor().GetOutsideValue();
-    }
+    /** Background value of the mask. Defaults to 0 */
+    itkGetConstMacro(BackgroundValue, MaskImagePixelType);
+    itkSetMacro(BackgroundValue, MaskImagePixelType);
 
-#ifdef ITK_USE_CONCEPT_CHECKING
-    /** Begin concept checking */
-    itkConceptMacro(MaskEqualityComparableCheck,
-        (Concept::EqualityComparable<typename TMaskImage::PixelType>));
-    itkConceptMacro(InputConvertibleToOutputCheck,
-        (Concept::Convertible<typename TInputImage::PixelType,
-                              typename TOutputImage::PixelType>));
-    /** End concept checking */
-#endif
+    /** Outside value. Defaults to 0 */
+    itkGetConstMacro(OutsideValue, OutputImagePixelType);
+    itkSetMacro(OutsideValue, OutputImagePixelType);
 
 protected:
-    MaskWithValueImageFilter() {}
-    virtual ~MaskWithValueImageFilter() {}
-
-    void PrintSelf(std::ostream &os, Indent indent) const
-    {
-        Superclass::PrintSelf(os, indent);
-        os << indent << "OutsideValue: "  << this->GetOutsideValue() << std::endl;
-    }
+    MaskWithValueImageFilter();
+    virtual ~MaskWithValueImageFilter();
+    void PrintSelf(std::ostream &os, Indent indent) const;
+    void ThreadedGenerateData(typename InputImageType::RegionType const & region, int thread_id);
 
 private:
     MaskWithValueImageFilter(const Self&); //purposely not implemented
     void operator=(const Self&); //purposely not implemented
+
+    MaskImagePixelType m_BackgroundValue;
+    OutputImagePixelType m_OutsideValue;
+
 }; // class MaskWithValueImageFilter
 
 } // namespace itk
 
+#include "itkMaskWithValueImageFilter.txx"
 
-#endif // medipy_components_segmentation_itkmaskwithvalueimagefilter_h
+#endif // b0f7acb3_a658_426b_9de9_f752e64f9468
