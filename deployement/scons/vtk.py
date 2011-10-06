43a981ca49c1a7a447fab499b3a84acd6822c247
Julien Lamy <lamy@unistra.fr>
2011-10-06 12:37:25 +0200

    deployement/scons/vtk : merge both init_file commands, allow nested modules

--- deployement/scons/vtk.py
+++ deployement/scons/vtk.py
@@ -134,21 +134,55 @@ def _possible_vtk_dirs() :
     return possible_vtk_dirs
 
 def datafile_command(target, source, env):
+    module_name = env["MODULE_NAME"].split(".")[-1]
+    
     data_file = open(target[0].path, "w")
-    data_file.write(env["MODULE_NAME"] + os.linesep)
+    data_file.write(module_name + os.linesep)
     for klass in env["CLASSES"] : 
         data_file.write(klass + os.linesep)
     data_file.close()
-    return None
-
+    
 def datafile_emitter(target, source, env):
-    target = [env["MODULE_NAME"]+".data"]
+    source_directory = os.path.dirname(
+        os.path.abspath(env["MODULE_NAME"].replace(".", os.path.sep)))
+    target = [os.path.join(source_directory, 
+                           env["MODULE_NAME"].split(".")[-1]+".data")]
+    return target, source    
+
+def module_init_command(target, source, env):
+    import subprocess
+    subprocess.call([env["VTK"]["vtk_wrap_python_init"],
+                    source[0].abspath, target[0].abspath])
+    
+    module_name = env["MODULE_NAME"].split(".")[-1]
+    if sys.platform != "win32" :
+        path = target[0].abspath
+        
+        file = open(path, "r")
+        lines = file.readlines()
+        file.close()
+        
+        new_lines = [line.replace("lib%s"%module_name, "%s"%module_name)
+                     for line in lines]
+        
+        file = open(path, "w")
+        file.writelines(new_lines)
+        file.close()
+
+def module_init_emitter(target, source, env):
+    source_directory = os.path.dirname(
+        os.path.abspath(env["MODULE_NAME"].replace(".", os.path.sep)))
+    target = [os.path.join(source_directory, 
+                           env["MODULE_NAME"].split(".")[-1]+"_init.cpp")]
     return target, source    
 
 def module_builder(env, module_name, classes, hint_file = ""):
     
+    source_directory = os.path.dirname(
+        os.path.abspath(module_name.replace(".", os.path.sep)))
+    
     if hint_file != "" :
-        hint_file = env.FindFile(hint_file, [""]).path
+        hint_file = env.FindFile(hint_file, [source_directory]).path
     
     # 1. find files related to classes
     source_suffixes = [".c", ".C", ".cxx", ".cpp", ".c++", ".cc"]
@@ -159,12 +193,12 @@ def module_builder(env, module_name, classes, hint_file = ""):
         files[klass] = {}
         
         for suffix in source_suffixes :
-            node = env.FindFile(klass+suffix, [""])
+            node = env.FindFile(klass+suffix, [source_directory])
             if node is not None :
                 files[klass]["source"] = node.abspath
                 
         for suffix in header_suffixes :
-            node = env.FindFile(klass+suffix, [""])
+            node = env.FindFile(klass+suffix, [source_directory])
             if node is not None :
                 files[klass]["header"] = node.abspath
 
@@ -177,37 +211,21 @@ def module_builder(env, module_name, classes, hint_file = ""):
     # 3. Data file
     data_file = env.VTKDataFile([x["header"] for x in files.values()], 
                                 CLASSES=classes, MODULE_NAME=module_name)
-    
+
     # 4. Python module init function
-    module_init = env.VTKModuleInit(module_name+"_init.cpp", data_file)
+    module_init = env.VTKModuleInit(data_file, MODULE_NAME=module_name)
     
-    # 5. Replace the function called initlib<module_name> by init<module_name>
-    def change_module_name(target, source, env):
-        print target[0].path, source[0].path
-        if sys.platform != "win32" :
-            path = source[0].path
-            
-            file = open(path, "r")
-            lines = file.readlines()
-            file.close()
-            
-            new_lines = [line.replace("lib%s"%module_name, "%s"%module_name)
-                         for line in lines]
-            
-            file = open(path, "w")
-            file.writelines(new_lines)
-            file.close()
-    env.Command("dummy", module_init, change_module_name)
-    
-    # 6. Python Module
+    # 5. Python Module
     sources = class_wrappers + module_init 
     for klass in files :
         if "source" in files[klass] :
-            sources.append(os.path.basename(files[klass]["source"]))
-    
-    env.PythonModule(module_name, sources, CPPPATH=env["VTK"]["CPPPATH"],
-                     LIBPATH = env["VTK"]["LIBPATH"],
-                     LIBS = env["VTK"]["LIBS"]+env["VTK"]["PYTHON_LIBS"])
+            sources.append(files[klass]["source"])
+
+    module = env.PythonModule(module_name, sources, 
+                              CPPPATH=env["VTK"]["CPPPATH"],
+                              LIBPATH = env["VTK"]["LIBPATH"],
+                              LIBS = env["VTK"]["LIBS"]+env["VTK"]["PYTHON_LIBS"])
+    return module
 
 def exists(env):
     return (configuration_variables() is not None)
@@ -222,7 +240,7 @@ def generate(env):
     env["BUILDERS"]["VTKDataFile"] = Builder(action=datafile_command,
                                              emitter=datafile_emitter)
     
-    module_init_command = "%s $SOURCE $TARGET"%env["VTK"]["vtk_wrap_python_init"]
-    env["BUILDERS"]["VTKModuleInit"] = Builder(action=module_init_command) 
+    env["BUILDERS"]["VTKModuleInit"] = Builder(action=module_init_command,
+                                               emitter=module_init_emitter) 
     
     env.AddMethod(module_builder, "vtkPythonModule")
