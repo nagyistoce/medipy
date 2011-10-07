5334c6c4b9ac3a6db78c6c4a3696adc7d84a9edb
Julien Lamy <lamy@unistra.fr>
2011-10-07 14:57:45 +0200

    deployement/scons/wrap_itk/WrappedClass : add typedefs and includes

--- deployement/scons/wrap_itk/wrapped_class.py
+++ deployement/scons/wrap_itk/wrapped_class.py
@@ -3,10 +3,11 @@ from type import Type
 from utils import filter_dimensions
 
 class WrappedClass(object):
-    def __init__(self, name, wrap_method="", instantiations=None):
+    def __init__(self, name, wrap_method="", instantiations=None, includes=None):
         self.name = name
         self.wrap_method = wrap_method
         self.instantiations = instantiations or []
+        self.includes = includes or ["{0}.h".format(Type(name).mangled_name)]
     
     def _get_wrap_method(self):
         return self._wrap_method
@@ -21,7 +22,29 @@ class WrappedClass(object):
                             wrap_method,", ".join("'{0}'".format(x) for x in wrap_methods)))
         self._wrap_method = wrap_method
     
+    def _get_typedefs(self):
+        typedefs = []
+        for instantiation in self.instantiations :
+            typedefs.append(Type(self.name, instantiation))
+
+            if "2_SUPERCLASSES" in self.wrap_method :
+                typedefs.append(Type(self.name, instantiation, "Superclass::Superclass"))
+                typedefs.append(Type(self.name, instantiation, "Superclass::Superclass::Pointer"))
+            
+            if "SUPERCLASS" in self.wrap_method :
+                typedefs.append(Type(self.name, instantiation, "Superclass"))
+                typedefs.append(Type(self.name, instantiation, "Superclass::Pointer"))
+            
+            if "POINTER" in self.wrap_method :
+                if self.wrap_method == "AUTOPOINTER" :
+                    typedefs.append(Type(self.name, instantiation, "SelfAutoPointer"))
+                else :
+                    typedefs.append(Type(self.name, instantiation, "Pointer"))
+        
+        return typedefs
+    
     wrap_method = property(_get_wrap_method, _set_wrap_method)
+    typedefs = property(_get_typedefs)
 
 def ImageFilter(types, parameters_count, dimensions_constraint=""):
     if dimensions_constraint :
