cd065362b05c759edb33a0468b3938f08e12f584
Julien Lamy <lamy@unistra.fr>
2011-10-07 14:57:12 +0200

    deployement/scons/wrap_itk/Library : add includes

--- deployement/scons/wrap_itk/library.py
+++ deployement/scons/wrap_itk/library.py
@@ -5,3 +5,18 @@ class Library(object):
         self.dependencies = dependencies or []
         self.linked_libraries = linked_libraries or []
         self.modules = modules or []
+    
+    def _get_includes(self):
+        return [
+            "itkCommand.h",
+            # itkStatisticsLabelObject.h does not exist in ITK 3.14
+            #"itkStatisticsLabelObject.h"
+            "itkOffset.h", "itkVector.h", "itkCovariantVector.h", "itkContinuousIndex.h", 
+            "itkArray.h", "itkFixedArray.h", "itkRGBPixel.h", "itkRGBAPixel.h", "complex",
+            "itkSymmetricSecondRankTensor.h", "itkImage.h", "itkVectorImage.h", 
+            "itkVariableLengthVector.h", "itkPoint.h", "itkLevelSetNode.h", 
+            "itkFlatStructuringElement.h", "itkSpatialObject.h", "itkHistogram.h", 
+            "itkLabelMap.h"]
+    
+    includes = property(_get_includes)
+    
