7f44b342de78d2764693f61438910a5b73e78c8b
Julien Lamy <lamy@unistra.fr>
2011-10-14 14:22:11 +0200

    lib/vtk : add vtkMisc to __init__

--- lib/medipy/vtk/__init__.py
+++ lib/medipy/vtk/__init__.py
@@ -7,7 +7,7 @@
 ##########################################################################
 
 from vtk_addons import (
-    vtkColorTransferFunctionWithAlpha, vtkEnhancedLookupTable,
+    vtkColorTransferFunctionWithAlpha, vtkEnhancedLookupTable, vtkMisc,
     vtkOrientationAnnotation)
 from bridge import (get_vtk_array_type, build_vtk_image, build_vtk_colormap,
                     build_lookup_table, build_color_transfer_function_with_alpha)
