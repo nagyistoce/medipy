b9431aae50c7dfcc67c15d18070a526a56165c66
Julien Lamy <lamy@unistra.fr>
2011-11-05 13:49:54 +0100

    lib/gui/base/HistoryPanel : use cursor api from History

--- lib/medipy/gui/base/history_panel.py
+++ lib/medipy/gui/base/history_panel.py
@@ -11,6 +11,11 @@ import wx
 import medipy.base
 
 class HistoryPanel(wx.Panel) :
+    """ GUI proxy to medipy.base.History. Earliest commands are displayed at the
+        top of the list, latest commands are displayed at the bottom of the
+        list. Clicking on a command will undo/redo up to this command.
+    """
+    
     def __init__(self, *args, **kwargs) :
         wx.Panel.__init__(self, *args, **kwargs)
         
@@ -20,7 +25,6 @@ class HistoryPanel(wx.Panel) :
         self.SetSizer(sizer)
         
         self._listbox.Bind(wx.EVT_LISTBOX, self.OnListBox)
-        self._previous_selection = None
         
         self._history = medipy.base.History()
         self._update_listbox()
@@ -28,18 +32,28 @@ class HistoryPanel(wx.Panel) :
     def add(self, command) :
         self._history.add(command)
         self._update_listbox()
-        self._listbox.SetSelection(self._listbox.GetCount()-1)
-        self._previous_selection = self._listbox.GetSelection()
+        self._listbox.SetSelection(self._listbox.GetCount()-1-self._history.cursor)
     
     def undo(self, count=1) : 
         self._history.undo(count)
-        self._listbox.SetSelection(self._previous_selection-count)
-        self._previous_selection = self._listbox.GetSelection()
+        self._listbox.SetSelection(self._listbox.GetCount()-1-self._history.cursor)
     
     def redo(self, count=1) :
         self._history.redo(count)
-        self._listbox.SetSelection(self._previous_selection+count)
-        self._previous_selection = self._listbox.GetSelection()
+        self._listbox.SetSelection(self._listbox.GetCount()-1-self._history.cursor)
+    
+    def _get_cursor(self) :
+        return self._history.cursor
+    
+    def _set_cursor(self, cursor) :
+        self._history.cursor = cursor
+        self._listbox.SetSelection(self._listbox.GetCount()-1-self._history.cursor)
+    
+    def _get_steps_count(self) :
+        return self._history.steps_count
+    
+    def _get_empty(self) :
+        return self._history.empty
     
     def _get_can_undo(self) :
         return self._history.can_undo
@@ -49,22 +63,18 @@ class HistoryPanel(wx.Panel) :
     
     def _get_labels(self) :
         return self._history.labels
-        
+    
+    cursor = property(_get_cursor, _set_cursor)
+    steps_count = property(_get_steps_count)
+    empty = property(_get_empty)    
     can_undo = property(_get_can_undo)
     can_redo = property(_get_can_redo)
     labels = property(_get_labels)
     
     def OnListBox(self, event) :
-        event.Skip()
         if self._listbox.GetSelection() == wx.NOT_FOUND :
             return
-        offset = self._listbox.GetSelection()-self._previous_selection
-        if offset >=0 :
-            function = self.redo
-        else :
-            offset = -offset
-            function = self.undo
-        function(offset)
+        self._history.cursor = self._listbox.GetCount()-1-self._listbox.GetSelection()
     
     def _update_listbox(self) :
         self._listbox.Clear()
