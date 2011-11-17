402540c28922904144239fca7d71972ed3bcbef2
Julien Lamy <lamy@unistra.fr>
2011-11-17 17:55:38 +0100

    plugins/morphology : add grayscale opening and closing

--- plugins/morphology/grayscale.py
+++ plugins/morphology/grayscale.py
@@ -107,3 +107,103 @@ def dilate_se(input, structuring_element):
     itk_output = filter()[0]
     output = medipy.itk.itk_image_to_medipy_image(itk_output, None, True)
     return output
+
+def open(input, *args, **kwargs):
+    """ Gray-scale opening of an image using a name of a structuring element and a
+        radius, or a structuring element.
+        
+        <gui>
+            <item name="input" type="Image" label="Input"/>
+            <item name="shape" type="Enum" initializer="('ball', 'box','cross')"
+                label="Shape"/>
+            <item name="radius" type="Int" initializer="1" label="Radius"/>
+            <item name="output" type="Image" initializer="output=True" role="return"
+                label="Output"/>
+        </gui>
+    """
+    
+    if len(args) == 1 :
+        return open_se(input, *args)
+    elif len(args) == 2 :
+        return open_shape_and_radius(input, **args)
+    elif len(args) == 0 :
+        if "shape" in kwargs :
+            return open_shape_and_radius(input, **kwargs)
+        elif "structuring_element" in kwargs :
+            return open_se(input, **kwargs)
+        else :
+            raise Exception("Incorrect parameters")
+    else :
+        raise Exception("Incorrect parameters")
+
+def open_shape_and_radius(input, shape, radius):
+    """ Gray-scale opening of an image using a name of a structuring element and a
+        radius
+    """
+    structuring_element = name_to_structuring_element(shape, input.ndim, radius)
+    return open_se(input, structuring_element)
+
+def open_se(input, structuring_element):
+    """ Gray-scale opening of an image using a structuring element.
+    """
+    
+    itk_input = medipy.itk.medipy_image_to_itk_image(input, False)
+    
+    erode_filter = itk.GrayscaleErodeImageFilter[itk_input, itk_input, structuring_element].New(
+        Input=itk_input, Kernel=structuring_element)
+    dilate_filter = itk.GrayscaleDilateImageFilter[itk_input, itk_input, structuring_element].New(
+        Input=erode_filter[0], Kernel=structuring_element)
+    
+    itk_output = dilate_filter()[0]
+    output = medipy.itk.itk_image_to_medipy_image(itk_output, None, True)
+    return output
+
+def close(input, *args, **kwargs):
+    """ Gray-scale closing of an image using a name of a structuring element and a
+        radius, or a structuring element.
+        
+        <gui>
+            <item name="input" type="Image" label="Input"/>
+            <item name="shape" type="Enum" initializer="('ball', 'box','cross')"
+                label="Shape"/>
+            <item name="radius" type="Int" initializer="1" label="Radius"/>
+            <item name="output" type="Image" initializer="output=True" role="return"
+                label="Output"/>
+        </gui>
+    """
+    
+    if len(args) == 1 :
+        return close_se(input, *args)
+    elif len(args) == 2 :
+        return close_shape_and_radius(input, **args)
+    elif len(args) == 0 :
+        if "shape" in kwargs :
+            return close_shape_and_radius(input, **kwargs)
+        elif "structuring_element" in kwargs :
+            return close_se(input, **kwargs)
+        else :
+            raise Exception("Incorrect parameters")
+    else :
+        raise Exception("Incorrect parameters")
+
+def close_shape_and_radius(input, shape, radius):
+    """ Gray-scale closing of an image using a name of a structuring element and a
+        radius
+    """
+    structuring_element = name_to_structuring_element(shape, input.ndim, radius)
+    return close_se(input, structuring_element)
+
+def close_se(input, structuring_element):
+    """ Gray-scale closing of an image using a structuring element.
+    """
+    
+    itk_input = medipy.itk.medipy_image_to_itk_image(input, False)
+    
+    dilate_filter = itk.GrayscaleDilateImageFilter[itk_input, itk_input, structuring_element].New(
+        Input=itk_input, Kernel=structuring_element)
+    erode_filter = itk.GrayscaleErodeImageFilter[itk_input, itk_input, structuring_element].New(
+        Input=dilate_filter[0], Kernel=structuring_element)
+    
+    itk_output = erode_filter()[0]
+    output = medipy.itk.itk_image_to_medipy_image(itk_output, None, True)
+    return output
