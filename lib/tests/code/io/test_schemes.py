f618d6e9d85828203c99a3676fc02d6ec25dbfab
Julien Lamy <lamy@unistra.fr>
2011-11-25 16:10:51 +0100

    Add url to loaded image metadata

--- lib/tests/code/io/test_schemes.py
+++ lib/tests/code/io/test_schemes.py
@@ -29,29 +29,33 @@ class TestSchemes(unittest.TestCase):
         path = os.path.join(self.data_directory, "avg152T1_LR_nifti.nii.gz")
         image = medipy.io.load(path)
         self.assertTrue(isinstance(image, medipy.base.Image))
+        self.assertEqual(image.metadata["loader"]["url"], path)
     
     def test_file(self) :
         url = "file:{0}".format(
             os.path.join(self.data_directory, "avg152T1_LR_nifti.nii.gz"))
         image = medipy.io.load(url)
         self.assertTrue(isinstance(image, medipy.base.Image))
+        self.assertEqual(image.metadata["loader"]["url"], url) 
     
     def test_dicomdir(self):
         location = self.extract_dicom()
         
         series_instance_uid = "1.3.46.670589.11.0.0.11.4.2.0.8743.5.5396.2006120114285654497"
         
-        url = "dicomdir:{0}".format(
-            os.path.join(location, "BRAINIX", "DICOMDIR"))
-        fragment = "{0}={1}".format("series_instance_uid", series_instance_uid)
-        image1 = medipy.io.load("{0}#{1}".format(url, fragment))
+        url = "dicomdir:{0}#series_instance_uid={1}".format(
+            os.path.join(location, "BRAINIX", "DICOMDIR"), series_instance_uid)
+        image1 = medipy.io.load(url)
         self.assertTrue(isinstance(image1, medipy.base.Image))
+        self.assertEqual(image1.metadata["loader"]["url"], url)
         
         tags = ["0x0020000e", "0020000e",
                 "(0020,000e)", "(0x0020,0x000e)"]
         for tag in tags :
-            fragment = "{0}={1}".format(tag, series_instance_uid)
-            image2 = medipy.io.load("{0}#{1}".format(url, fragment))
+            url = "dicomdir:{0}#{1}={2}".format(
+                os.path.join(location, "BRAINIX", "DICOMDIR"), 
+                tag, series_instance_uid)
+            image2 = medipy.io.load(url)
             numpy.testing.assert_array_equal(image1, image2)
         
         self.clean_dicom(location)
@@ -63,14 +67,17 @@ class TestSchemes(unittest.TestCase):
             os.path.join(location, "BRAINIX", "2182114", "401"))
         image1 = medipy.io.load(url)
         self.assertTrue(isinstance(image1, medipy.base.Image))
+        self.assertEqual(image1.metadata["loader"]["url"], url)
         
         tags = ["series_instance_uid", 
                 "0x0020000e", "0020000e",
                 "(0020,000e)", "(0x0020,0x000e)"]
         series_instance_uid = "1.3.46.670589.11.0.0.11.4.2.0.8743.5.5396.2006120114285654497"
         for tag in tags :
-            fragment = "{0}={1}".format(tag, series_instance_uid)
-            image2 = medipy.io.load("{0}#{1}".format(url, fragment))
+            url = "dicom:{0}#{1}={2}".format(
+                os.path.join(location, "BRAINIX", "2182114", "401"), 
+                tag, series_instance_uid)
+            image2 = medipy.io.load(url)
             numpy.testing.assert_array_equal(image1, image2)
         
         self.clean_dicom(location)
