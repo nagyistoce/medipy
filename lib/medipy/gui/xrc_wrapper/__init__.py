a20d6a892cfe1090a62e487466bc83807240c923
Julien Lamy <lamy@unistra.fr>
2012-02-15 15:53:30 +0100

    lib/gui/control : add coordinates

--- lib/medipy/gui/xrc_wrapper/__init__.py
+++ lib/medipy/gui/xrc_wrapper/__init__.py
@@ -1,9 +1,9 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011             
-# Distributed under the terms of the CeCILL-B license, as published by 
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
-# for details.                                                      
+# MediPy - Copyright (C) Universite de Strasbourg, 2011-2012
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
 ##########################################################################
 
 from medipy.gui.xrc_wrapper.dialog import Dialog
@@ -19,6 +19,7 @@ from medipy.gui.xrc_wrapper.timectrl_xml_handler import TimeCtrlXMLHandler
 
 from medipy.gui.xrc_wrapper.bool_xml_handler import BoolXMLHandler
 from medipy.gui.xrc_wrapper.control_image_xml_handler import ControlImageXMLHandler
+from medipy.gui.xrc_wrapper.coordinates_xml_handler import CoordinatesXMLHandler
 from medipy.gui.xrc_wrapper.dicom_explorer_xml_handler import DICOMExplorerXMLHandler
 from medipy.gui.xrc_wrapper.directory_xml_handler import DirectoryXMLHandler
 from medipy.gui.xrc_wrapper.file_xml_handler import FileXMLHandler
