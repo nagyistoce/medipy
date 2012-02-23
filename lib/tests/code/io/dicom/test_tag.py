98417ccbf2f7e442533fe28d3cdb6e4b8c52a90f
Julien Lamy <lamy@unistra.fr>
2012-02-23 10:23:40 +0100

    lib/io/dicom/Tag : add __eq__ and __ne__

--- lib/tests/code/io/dicom/test_tag.py
+++ lib/tests/code/io/dicom/test_tag.py
@@ -22,6 +22,20 @@ class TestTag(unittest.TestCase):
         
         private_tag = medipy.io.dicom.Tag(0x0029,0x1010)
         self.assertTrue(private_tag.private)
+    
+    def test_equality(self):
+        tag1 = medipy.io.dicom.Tag(0x00100010)
+        tag2 = medipy.io.dicom.Tag(0x00100010)
+        tag3 = medipy.io.dicom.Tag(0x00100020)
+        
+        self.assertEqual(tag1, tag2)
+        self.assertNotEqual(tag1, tag3)
+        
+        self.assertEqual(tag1, (0x0010,0x0010))
+        self.assertEqual(tag1, 0x00100010)
+        
+        self.assertNotEqual(tag1, (0x0010,0x0020))
+        self.assertNotEqual(tag1, 0x00100020)
 
 if __name__ == '__main__':
     unittest.main()
