3c91fa17fe3ac82999479228042ca18f773822a0
Vincent Noblet <vincent.noblet@unistra.fr>
2012-02-28 15:08:36 +0100

    Ajout d'une fonction de normalisation d'intensite (en fait, c'est la meme que celle wrappee de medimax, mais codee en 2 ligne en python). C'etait juste pour faire un test et verifier l'implantation de medimax

--- plugins/intensity/api.py
+++ plugins/intensity/api.py
@@ -1,3 +1,4 @@
 from normalization import mean_stdev_normalization
+from normalization import one_parameter_linear_regression_normalization
 from otsu import otsu
 from threshold import binary_threshold, threshold
