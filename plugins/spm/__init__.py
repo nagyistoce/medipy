07f6c23453709ece1b5e184b31687aa727c1a357
Julien Lamy <lamy@unistra.fr>
2012-05-16 12:17:16 +0200

    plugins/spm : add ResultReport

--- plugins/spm/__init__.py
+++ plugins/spm/__init__.py
@@ -4,3 +4,4 @@ from contrast_manager import ContrastManager
 from factorial_design import FactorialDesign
 from model_estimation import ModelEstimation
 from new_segment import NewSegment
+from result_report import ResultReport
