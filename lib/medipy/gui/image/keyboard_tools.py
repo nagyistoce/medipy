49b6b31859defb6e2cad0b9451079cdcbfd6faa8
Julien Lamy <lamy@unistra.fr>
2012-06-22 16:16:01 +0200

    lib/gui/image/keyboard_tools : use image.direction in MoveCursor

--- lib/medipy/gui/image/keyboard_tools.py
+++ lib/medipy/gui/image/keyboard_tools.py
@@ -7,22 +7,13 @@
 ##########################################################################
 
 import numpy
+
+import medipy.base.array
 from tools import KeyboardTool
 
 class MoveCursor(KeyboardTool):
     """ Move the cursor on the slice using the arrows keys and Page Up/Page Down.
     """
-    
-    motion = {
-        "Left" : numpy.asarray((0,0,-1)),
-        "Right" : numpy.asarray((0,0,1)),
-        "Up" : numpy.asarray((0,1,0)),
-        "Down" : numpy.asarray((0,-1,0)),
-        "Prior" : numpy.asarray((1,0,0)),
-        "PageUp" : numpy.asarray((1,0,0)),
-        "Next" : numpy.asarray((-1,0,0)),
-        "PageDown" : numpy.asarray((-1,0,0)),
-    }
     
     def __init__(self):
         super(MoveCursor, self).__init__()
@@ -30,15 +21,35 @@ class MoveCursor(KeyboardTool):
     def press(self, rwi, slice):
         
         image = slice.layers[0].image
-        ndim = image.ndim
         
-        motion = self.motion[rwi.GetKeySym()][-ndim:]
+        world_to_slice = medipy.base.array.reshape(slice.world_to_slice, 
+            (image.ndim, image.ndim), "constant", False, value=0)
+        # Add ones on the diagonal when necessary
+        for rank in range(image.ndim) :
+            if numpy.less_equal(slice.world_to_slice.shape, rank).all() : 
+                world_to_slice[image.ndim-rank-1, image.ndim-rank-1] = 1.
+        
+        if slice.display_coordinates in ["physical", "nearest_axis_aligned"] :
+            matrix = numpy.dot(world_to_slice, image.direction)
+        else :
+            matrix = world_to_slice
+        
+        if rwi.GetKeySym() == "Left" :
+            motion = -matrix[-1]
+        elif rwi.GetKeySym() == "Right" :
+            motion = matrix[-1]
+        elif rwi.GetKeySym() == "Up" :
+            motion = matrix[-2]
+        elif rwi.GetKeySym() == "Down" :
+            motion = -matrix[-2]
+        elif rwi.GetKeySym() in ["Prior", "PageUp"] :
+            motion = matrix[-3]
+        elif rwi.GetKeySym() in ["Next", "PageDown"] :
+            motion = -matrix[-3]
+        else :
+            motion = [0]*image.ndim
     
-        slice_position = numpy.dot(slice.world_to_slice, 
-                                   slice.cursor_index_position)
-        slice_position = numpy.add(slice_position, motion)
-        
-        image_position = numpy.dot(slice.slice_to_world, slice_position)
+        image_position = numpy.add(slice.cursor_index_position, motion)
         
         if image.is_inside(image_position) :
             slice.cursor_index_position = image_position
