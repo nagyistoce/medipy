71f843c3d155abd1e634265a5364b0e3e7551d40
Julien Lamy <lamy@unistra.fr>
2012-07-26 15:38:05 +0200

    deployement/package_wrapitk.sh : fix hardcoding of Python and CMake versions

--- deployement/package_wrapitk.sh
+++ deployement/package_wrapitk.sh
@@ -98,10 +98,12 @@ cmake -D CMAKE_INSTALL_PREFIX=local_install/usr -P cmake_install.cmake
 sed -e "s/^#FILE/FILE/" -i install_wrapitk_compatibility.cmake
 
 # Install additional files
-mkdir -p local_install/usr/lib/python2.6/dist-packages
-mkdir -p local_install/usr/share/cmake-2.6/Modules
-cp Languages/Python/InstallOnly/WrapITK.pth local_install/usr/lib/python2.6/dist-packages
-cp InstallOnly/FindWrapITK.cmake local_install/usr/share/cmake-2.6/Modules
+PYTHON_VERSION=`python -c "import sys; print '{0}.{1}'.format(*sys.version_info[:2])"`
+CMAKE_ROOT=`echo 'message(${CMAKE_ROOT})' | cmake -P /dev/stdin`
+mkdir -p local_install/usr/lib/python${PYTHON_VERSION}/dist-packages
+mkdir -p local_install/${CMAKE_ROOT}/Modules
+cp Languages/Python/InstallOnly/WrapITK.pth local_install/usr/lib/python${PYTHON_VERSION}/dist-packages
+cp InstallOnly/FindWrapITK.cmake local_install/${CMAKE_ROOT}/Modules
 
 # Create binary package control file
 mkdir -p local_install/DEBIAN
