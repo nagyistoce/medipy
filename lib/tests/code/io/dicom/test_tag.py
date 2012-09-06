7c30f527175bd854e2b0297d2fab139b65495c41
Julien Lamy <lamy@unistra.fr>
2012-09-06 16:53:23 +0200

    lib/tests/code/io/dicom/test_tag : add test for tags higher than (7fff,ffff)

--- lib/tests/code/io/dicom/test_tag.py
+++ lib/tests/code/io/dicom/test_tag.py
@@ -10,10 +10,24 @@ class TestTag(unittest.TestCase):
         self.assertEqual(tag.element, 0x0020)
         self.assertFalse(tag.private)
     
+    def test_high_from_int(self):
+        # Test tag > (7fff,ffff) (i.e. > 2^31) for 32 bits systems
+        tag = medipy.io.dicom.Tag(0xfffee00d)
+        self.assertEqual(tag.group, 0xfffe)
+        self.assertEqual(tag.element, 0xe00d)
+        self.assertFalse(tag.private)
+    
     def test_from_tuple(self):
         tag = medipy.io.dicom.Tag(0x0010,0x0020)
         self.assertEqual(tag.group, 0x0010)
         self.assertEqual(tag.element, 0x0020)
+        self.assertFalse(tag.private)
+    
+    def test_high_from_tuple(self):
+        # Test tag > (7fff,ffff) (i.e. > 2^31) for 32 bits systems
+        tag = medipy.io.dicom.Tag(0xfffe, 0xe00d)
+        self.assertEqual(tag.group, 0xfffe)
+        self.assertEqual(tag.element, 0xe00d)
         self.assertFalse(tag.private)
     
     def test_private(self):
