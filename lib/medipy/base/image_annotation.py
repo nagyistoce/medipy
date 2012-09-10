ee668045e123f8ea61259caf026e21b1b9422c41
Julien Lamy <lamy@unistra.fr>
2012-09-10 18:00:43 +0200

    lib/base/ImageAnnotation : add XML serialization ; add corresponding test

--- lib/medipy/base/image_annotation.py
+++ lib/medipy/base/image_annotation.py
@@ -1,13 +1,15 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011             
-# Distributed under the terms of the CeCILL-B license, as published by 
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
-# for details.                                                      
+# MediPy - Copyright (C) Universite de Strasbourg, 2011-2012
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
 ##########################################################################
 
 import logging
+import xml.etree.ElementTree
 
+from exception import Exception
 from observable import Observable
 
 class ImageAnnotation(Observable):
@@ -28,6 +30,14 @@ class ImageAnnotation(Observable):
         cube = 1
         cross = 2
         point = 3
+        
+        @staticmethod
+        def to_name(value):
+            dictionary = dict([(getattr(ImageAnnotation.Shape, name), name) 
+                               for name in dir(ImageAnnotation.Shape) 
+                               if isinstance(getattr(ImageAnnotation.Shape, name), int)])
+            return dictionary[value]
+            
     
     def __init__(self, position = None, label = None, shape = None, size = None,
         color = None, filled = None, comment = None) :
@@ -53,3 +63,57 @@ class ImageAnnotation(Observable):
             self.notify_observers(attr, old_value = old_value)
         else :
             Observable.__setattr__(self, attr, value)
+    
+    def to_xml(self):
+        root = xml.etree.ElementTree.Element("ImageAnnotation")
+        
+        position = xml.etree.ElementTree.SubElement(root, "position")
+        position.text = " ".join([str(x) for x in self.position])
+        
+        label = xml.etree.ElementTree.SubElement(root, "label")
+        label.text = self.label.encode("utf-8")
+        
+        shape = xml.etree.ElementTree.SubElement(root, "shape")
+        shape.text = ImageAnnotation.Shape.to_name(self.shape)
+        
+        size = xml.etree.ElementTree.SubElement(root, "size")
+        size.text = str(self.size)
+        
+        color = xml.etree.ElementTree.SubElement(root, "color")
+        color.text = " ".join([str(x) for x in self.color])
+        
+        filled = xml.etree.ElementTree.SubElement(root, "filled")
+        filled.text = str(self.filled)
+        
+        comment = xml.etree.ElementTree.SubElement(root, "comment")
+        comment.text = self.comment.encode("utf-8")
+        
+        return root
+    
+    @staticmethod
+    def from_xml(root):
+        if root.tag != "ImageAnnotation" :
+            raise Exception("Cannot build an ImageAnnotation from a {0}".format(repr(root.tag)))
+        
+        annotation = ImageAnnotation()
+        
+        child_tags = dict([(x.tag,x) for x in root.getchildren()])
+        if "position" in child_tags :
+            annotation.position = [
+                float(x) for x in child_tags["position"].text.split(" ")]
+        if "label" in child_tags :
+            annotation.label = child_tags["label"].text.decode("utf-8")
+        if "shape" in child_tags :
+            annotation.shape = getattr(ImageAnnotation.Shape, child_tags["shape"].text)
+        if "size" in child_tags :
+            annotation.size = float(child_tags["size"].text)
+        if "color" in child_tags :
+            annotation.color = [
+                float(x) for x in child_tags["color"].text.split(" ")]
+        if "filled" in child_tags :
+            annotation.filled = (child_tags["filled"].text.lower() == "true")
+        if "comment" in child_tags :
+            annotation.comment = child_tags["comment"].text.decode("utf-8")
+        
+        return annotation
+        
---
+++ lib/tests/code/base/test_image_annotation.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy
+import medipy.base
+
+class TestImageAnnotation(unittest.TestCase):
+    def test_xml(self) :
+        annotation = medipy.base.ImageAnnotation(
+            (1, 2.718, 3.14), "Something", 
+            medipy.base.ImageAnnotation.Shape.cube, 10, (0.5,0.2,0.3), True, 
+            "lorem\nipsum")
+        data = annotation.to_xml()
+        other_annotation = medipy.base.ImageAnnotation.from_xml(data)
+        
+        numpy.testing.assert_equal(annotation.position, other_annotation.position)
+        self.assertEqual(annotation.label, other_annotation.label)
+        self.assertEqual(annotation.shape, other_annotation.shape)
+        self.assertEqual(annotation.size, other_annotation.size)
+        numpy.testing.assert_equal(annotation.color, other_annotation.color)
+        self.assertEqual(annotation.filled, other_annotation.filled)
+        self.assertEqual(annotation.comment, other_annotation.comment)
+
+if __name__ == '__main__':
+    unittest.main()
