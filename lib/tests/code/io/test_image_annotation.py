bce211d36285f299375554d327a16a71abbe6b6f
Julien Lamy <lamy@unistra.fr>
2012-09-12 17:41:35 +0200

    lib/io/image_annotation : add XML serialization for list of annotations

--- lib/tests/code/io/test_image_annotation.py
+++ lib/tests/code/io/test_image_annotation.py
@@ -20,6 +20,31 @@ class TestImageAnnotation(unittest.TestCase):
         numpy.testing.assert_equal(annotation.color, other_annotation.color)
         self.assertEqual(annotation.filled, other_annotation.filled)
         self.assertEqual(annotation.comment, other_annotation.comment)
+    
+    def test_list_xml(self) :
+        annotation1 = medipy.base.ImageAnnotation(
+            (1, 2.718, 3.14), "Something", 
+            medipy.base.ImageAnnotation.Shape.cube, 10, (0.5,0.2,0.3), True, 
+            "lorem\nipsum")
+        annotation2 = medipy.base.ImageAnnotation(
+            (42, 1.2345, 6.789), "Nothing", 
+            medipy.base.ImageAnnotation.Shape.cross, 20, (0.1,0.9,0.7), False, 
+            "ipsum\nlorem")
+        
+        annotations = [annotation1, annotation2]
+        data = medipy.io.image_annotation.annotations_to_xml(annotations)
+        other_annotations = medipy.io.image_annotation.annotations_from_xml(data)
+        
+        self.assertEqual(len(annotations), len(other_annotations))
+        for index, annotation in enumerate(annotations) :
+            other_annotation = other_annotations[index] 
+            numpy.testing.assert_equal(annotation.position, other_annotation.position)
+            self.assertEqual(annotation.label, other_annotation.label)
+            self.assertEqual(annotation.shape, other_annotation.shape)
+            self.assertEqual(annotation.size, other_annotation.size)
+            numpy.testing.assert_equal(annotation.color, other_annotation.color)
+            self.assertEqual(annotation.filled, other_annotation.filled)
+            self.assertEqual(annotation.comment, other_annotation.comment)
 
 if __name__ == '__main__':
     unittest.main()
