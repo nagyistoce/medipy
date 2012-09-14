35fb3d07edc47d4c6bb865205abf6644e106bb70
Julien Lamy <lamy@unistra.fr>
2012-09-14 17:34:10 +0200

    lib/gui/image/slice : fix _get_key_name on Ubuntu 12.04

--- lib/medipy/gui/image/slice.py
+++ lib/medipy/gui/image/slice.py
@@ -861,7 +861,7 @@ class Slice(PropertySynchronized) :
                 self._mouse_tools[source].dispatch_interaction(rwi, self)
     
     def _get_key_name(self, rwi):
-        if rwi.GetKeyCode() == "\x00" : 
+        if rwi.GetKeyCode() in ["\x00", ""] : 
             key = rwi.GetKeySym()
         else :
             key = rwi.GetKeyCode()	
