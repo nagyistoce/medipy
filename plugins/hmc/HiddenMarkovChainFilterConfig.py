d3a26bb93abf5c7cf3289efe0372883890138ec8
Fran?ois Lavigne <f.lavigne@unistra.fr>
2012-09-21 14:42:12 +0200

    hmc files

---
+++ plugins/hmc/HiddenMarkovChainFilterConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'Base',)
+templates = (
+    ('HiddenMarkovChainFilter', 'itk::HiddenMarkovChainFilter', 'itkHiddenMarkovChainFilterIF3IF3', 'itk::Image< float,3 >, itk::Image< float,3 >'),
+)
