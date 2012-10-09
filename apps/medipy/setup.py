5d0e0d7b40b496ee74929245f23b461cdbd431f0
Julien Lamy <lamy@unistra.fr>
2012-10-09 16:47:01 +0200

    apps/medipy/setup : add api files

--- apps/medipy/setup.py
+++ apps/medipy/setup.py
@@ -7,8 +7,10 @@ if __name__ == "__main__" :
     medipy_plugins = [entry 
                       for entry in os.listdir(plugins_root)
                       if os.path.isdir(os.path.join(plugins_root, entry)) and
+                         os.path.isfile(os.path.join(plugins_root, entry, "api.py")) and
                          entry != "medimax"
                      ]
+    includes.extend(["medipy.{0}.api".format(entry) for entry in medipy_plugins])
     if "hsqc" in medipy_plugins :
         # TODO : move this to the plugin directory
         includes.extend([
