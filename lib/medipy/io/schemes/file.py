5bd033b8097b593cbcc6c2d839f085bc6dcfff16
lamy <lamy@localhost>
2012-10-12 18:00:19 +0200

    lib/io/schemes/file : move NIfTI bvec/bval processing to medipy.io.nifti_io

--- lib/medipy/io/schemes/file.py
+++ lib/medipy/io/schemes/file.py
@@ -48,42 +48,16 @@ def load_serie(path, fragment=None, loader=None) :
     norigin = image.origin[1:]
     nspacing = image.spacing[1:]
     ndirection = image.direction[1:,1:]
-    for ndata in list(image.data) :
-        args = { "loader" : image.metadata["loader"] }
-        limages.append(Image(data=ndata,origin=norigin,spacing=nspacing,direction=ndirection,metadata=args))
-
-    # DWI nifti
-    name = path.split('/')[-1].split('.')[0]
-    base = "/".join(path.split('/')[:-1])+"/"
-    ext = "."+".".join(path.split('/')[-1].split('.')[1:])
-    print base,ext,name
-    N = len(limages)
-    if ext==".nii" or ext==".nii.gz" :
-        lfbvec = [base+name+".bvecs",base+name+".bvec",base+"bvecs",base+"bvec"]
-        lfbval = [base+name+".bvals",base+name+".bval",base+"bvals",base+"bval"]
-        kbvec = [os.path.isfile(f) for f in lfbvec]
-        kbval = [os.path.isfile(f) for f in lfbval]
-        k1 = None
-        if True in kbvec :
-            k1 = kbvec.index(True)
-        k2 = None
-        if True in kbval :
-            k2 = kbval.index(True)
-        if k1!=None and k2!=None :
-            fbvec = lfbvec[k1]
-            fbval = lfbval[k2]
-            bval = np.array(np.loadtxt(fbval))
-            grad = np.array(np.loadtxt(fbvec),dtype=np.single)
-            grad = grad.T
-            if N==bval.shape[0] and N==grad.shape[0] :
-                for cnt in range(N) :
-                    dwi_dataset = DataSet()
-                    dwi_dataset.diffusion_directionality = "DIRECTIONAL"
-                    dwi_dataset.diffusion_bvalue = bval[cnt]
-                    gradient_dataset = DataSet()
-                    gradient_dataset.diffusion_gradient_orientation = grad[cnt]
-                    dwi_dataset.diffusion_gradient_direction_sequence = [gradient_dataset]
-                    limages[cnt].metadata["mr_diffusion_sequence"] = [dwi_dataset]
+    for index in range(image.shape[0]) :
+        data = image[index:]
+        
+        metadata = { "loader" : image.metadata["loader"] }
+        if "mr_diffusion_sequence" in image.metadata :
+            diffusion_data = image.metadata["mr_diffusion_sequence"][index]
+            metadata["mr_diffusion_sequence"] = [diffusion_data]
+            
+        limages.append(Image(data=data,origin=norigin,spacing=nspacing,
+                             direction=ndirection,metadata=metadata))
 
     return limages
 
