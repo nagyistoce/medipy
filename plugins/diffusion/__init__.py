1c2ddcbaa83633b58f5642be5fbae4f8f04d1718
Antoine Grigis <grigis@unistra.fr>
2012-10-17 17:16:31 +0200

    plugins/diffusion/itkSymmetricSpectralAnalysisImageFilter : multithread spectral decomposition

--- plugins/diffusion/__init__.py
+++ plugins/diffusion/__init__.py
@@ -1,4 +1,5 @@
 import os.path
 import medipy.itk
 
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "SymmetricSpectralAnalysisImageFilter")
 medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "SecondOrderSymmetricTensorReconstructionFilter")
