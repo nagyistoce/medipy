36f6def81d997a5b6ba7e15ee2eb693a57289ae4
Antoine Grigis <grigis@unistra.fr>
2012-10-18 09:38:49 +0200

    plugins/diffusion/itkMeanDiffusivityImageFilter : multithread mean diffusivity image filter

--- plugins/diffusion/__init__.py
+++ plugins/diffusion/__init__.py
@@ -3,3 +3,5 @@ import medipy.itk
 
 medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "SymmetricSpectralAnalysisImageFilter")
 medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "SecondOrderSymmetricTensorReconstructionFilter")
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MeanDiffusivityImageFilter")
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "FractionalAnisotropyImageFilter")
