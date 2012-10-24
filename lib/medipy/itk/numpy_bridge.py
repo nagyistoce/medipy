346912aab4e67ad435f9b2c01d23e2abd290886a
Julien Lamy <lamy@unistra.fr>
2012-10-24 16:46:58 +0200

    lib/itk/numpy_bridge : fix itk_image_to_medipy_image for vector images

--- lib/medipy/itk/numpy_bridge.py
+++ lib/medipy/itk/numpy_bridge.py
@@ -165,13 +165,12 @@ def itk_image_to_medipy_image(itk_image, medipy_image, transferOwnership):
         if not itk.NumpyBridge[itk_image].IsBufferShared(medipy_image.data, itk_image) :
             medipy_image.data = itk_image_to_array(itk_image, transferOwnership)
         medipy_image.data_type = "scalar"
-        matrix_type = itk.Matrix[itk.D, len(medipy_image.shape), len(medipy_image.shape)]
     elif itk_image.GetNameOfClass() == "VectorImage" :
         if not itk.NumpyBridge[itk_image].IsBufferShared(medipy_image.data, itk_image) :
             medipy_image.data = itk_vector_image_to_array(itk_image, transferOwnership)
         medipy_image.data_type = "vector"
-        matrix_type = itk.Matrix[itk.D, len(medipy_image.shape)-1, len(medipy_image.shape)-1]    
-
+    
+    matrix_type = itk.Matrix[itk.D, medipy_image.ndim, medipy_image.ndim]    
     matrix_bridge = itk.MatrixBridge[matrix_type]
     itk_direction = matrix_bridge.GetArrayFromMatrix(itk_image.GetDirection())
     medipy_image.direction = numpy.fliplr(numpy.flipud(itk_direction))
