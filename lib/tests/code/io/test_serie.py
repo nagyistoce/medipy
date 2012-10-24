74b9ad3825f05c85c022829b45807ac71b766061
Julien Lamy <lamy@unistra.fr>
2012-10-24 11:21:28 +0200

    lib/io : fix load_series

--- lib/tests/code/io/test_serie.py
+++ lib/tests/code/io/test_serie.py
@@ -46,6 +46,25 @@ class TestSerie(unittest.TestCase):
         images = medipy.io.load_serie(filename)
         self._test_dti_serie(images)
     
+    def test_save_nifti(self):
+        # Load the DICOM
+        
+        tempdir = self.extract("ep2d_DTI_20_dir_RL_pitch_yaw_22.tar.bz2")
+        
+        series_instance_uid = "1.3.12.2.1107.5.2.32.35389.2010072309200364977723827.0.0.0"
+        url = "dicom:{0}#series_instance_uid={1}".format(
+            tempdir, series_instance_uid)
+        dicom_images = medipy.io.load_serie(url, None)
+        
+        self.cleanup(tempdir)
+        
+        tempdir = tempfile.mkdtemp()
+        medipy.io.save_serie(dicom_images, os.path.join(tempdir, "foo.nii.gz"))
+        nifti_images = medipy.io.load_serie(os.path.join(tempdir, "foo.nii.gz"))
+        self.cleanup(tempdir)
+        
+        self._test_dti_serie(nifti_images)
+    
     #####################
     # Private interface #
     #####################    
