f5b085d65ef49c79dcd9d1becc72aa975205af8d
Julien Lamy <lamy@unistra.fr>
2012-11-06 11:55:08 +0100

    lib/gui/xrc_wrapper : fix import problem

--- lib/medipy/gui/xrc_wrapper/directory_xml_handler.py
+++ lib/medipy/gui/xrc_wrapper/directory_xml_handler.py
@@ -1,15 +1,13 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011             
-# Distributed under the terms of the CeCILL-B license, as published by 
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
-# for details.                                                      
+# MediPy - Copyright (C) Universite de Strasbourg
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
 ##########################################################################
 
 import wx
 import wx.xrc
-
-from medipy.gui.control import Directory
 
 class DirectoryXMLHandler(wx.xrc.XmlResourceHandler):
     def __init__(self):
@@ -20,6 +18,9 @@ class DirectoryXMLHandler(wx.xrc.XmlResourceHandler):
         return self.IsOfClass(node, "medipy.gui.control.Directory")
     
     def DoCreateResource(self):
+        # Late import to avoid circular dependencies
+        from medipy.gui.control import Directory
+        
         assert self.GetInstance() is None
         control = Directory(
             parent = self.GetParentAsWindow(),
