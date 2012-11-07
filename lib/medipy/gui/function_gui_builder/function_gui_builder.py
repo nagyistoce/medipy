872cd392e14288c0d757bcf4c77ad5cde54f55eb
Julien Lamy <lamy@unistra.fr>
2012-11-07 09:34:52 +0100

    lib/gui/function_gui_builder : replace explicit reference to Application by observers

--- lib/medipy/gui/function_gui_builder/function_gui_builder.py
+++ lib/medipy/gui/function_gui_builder/function_gui_builder.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011-2012
+# MediPy - Copyright (C) Universite de Strasbourg
 # Distributed under the terms of the CeCILL-B license, as published by
 # the CEA-CNRS-INRIA. Refer to the LICENSE file or to
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -17,13 +17,13 @@ import numpy
 import wx
 
 import medipy.gui.control
-from medipy.base import Image, Object3D, ObservableList
+from medipy.base import Image, Object3D, Observable, ObservableList
 from medipy.gui import PeriodicProgressDialog, WorkerThread
 from medipy.gui.viewer_3d_frame import Viewer3DFrame
 
 from builder import parse_docstring
 
-class FunctionGUIBuilder(object):
+class FunctionGUIBuilder(Observable):
     """ Create the GUI of a function based on its docstring.
     
         When clicking the "Run" button, the function will be called with arguments
@@ -31,6 +31,12 @@ class FunctionGUIBuilder(object):
     """
     
     def __init__(self, parent, function, images, viewer_3ds, config_path=None):
+        
+        # The observers of these events are notified when the FunctionGUIBuilder
+        # has to modify its images/viewer_3ds as a result of the user choices
+        # in the GUI.
+        Observable.__init__(self, 
+            ["new_image", "replace_image", "new_viewer_3d", "set_image_to_object_3d"])
         
         self.panel = wx.Panel(parent)
         
@@ -52,7 +58,7 @@ class FunctionGUIBuilder(object):
         # Controls
         controls_sizer = wx.BoxSizer(wx.VERTICAL)
         self._create_gui(controls_sizer)
-        self._load_parameters()
+#        self._load_parameters()
         # Main layout
         panel_sizer = wx.BoxSizer(wx.VERTICAL)
         panel_sizer.Add(controls_sizer, flag=wx.EXPAND)
@@ -68,7 +74,7 @@ class FunctionGUIBuilder(object):
     def __call__(self):
         
         # Save the current value of the parameters
-        self._save_parameters()
+#        self._save_parameters()
         
         # Build the expression namespace
         namespace = {"function" : self._function}
@@ -132,15 +138,16 @@ class FunctionGUIBuilder(object):
                 # Special case for Image and Object3D
                 if parameter["type"] == "Image" :
                     if control.output_checked :
-                        wx.GetApp().append_image(value)
+                        self.notify_observers("new_image", image=value)
                     else :
-                        index = wx.GetApp().images.index(control.value)
-                        wx.GetApp().close_image(wx.GetApp().gui_images[index])
-                        wx.GetApp().insert_image(index, value)
+                        self.notify_observers("replace_image", 
+                            old=control.value, new=value)
                 elif parameter["type"] == "Object3D" :
                     if control.output_checked :
                         viewer = Viewer3DFrame(parent=None, objects_3d = ObservableList())
                         viewer.Show()
+                        # TODO
+                        # self.notify_observers("new_viewer_3d", viewer=viewer)
                         wx.GetApp().append_viewer_3d(viewer)
                         control.value = self._viewer_3ds[-1]
                     viewer_3d = control.value 
@@ -148,16 +155,27 @@ class FunctionGUIBuilder(object):
                     # If object has an associated image, set the GUI image
                     image = value.image
                     if image is not None :
+                        #self.notify_observers("set_image_to_object_3d", image=XXX, object_3d=YYY)
                         index = wx.GetApp().images.index(image)
                         value.gui_image = wx.GetApp().gui_images[index]
                     if len(viewer_3d.objects_3d) == 1 :
                         viewer_3d.view_all()
                         viewer_3d.update_object_editor()
                 
-                # In any case but Object3D (whose value is a Viewe3D),
+                # In any case but Object3D (whose value is a Viewer3D),
                 # update the control
                 if parameter["type"] != "Object3D" :
                     control.value = value
+#        
+#        for parameter in self._parameters :
+#            if parameter["type"] != "Image" :
+#                continue
+#            
+#            name = parameter["name"]
+#            control = self._controls[name]
+#            # Choices may have changed
+#            if not control.validate() :
+#                control.value = self._images[0]
     
     def validate_form(self):
         all_controls_valid = True
@@ -263,71 +281,71 @@ class FunctionGUIBuilder(object):
         
         self.validate_form()
 
-    def _get_identifier(self, function):
-        """ Return the unique identifier for given function (used in 
-            _load_parameters and _save_parameters).
-        """
-        
-        identifier = "/".join([
-            sys.modules[function.__module__].__file__, function.__name__])
-        identifier = hashlib.md5(identifier).hexdigest()
-        
-        return identifier
-
-    def _load_parameters(self):
-        """ Load the previously-saved values of the parameters (when possible).
-        """
-        
-        if self.config_path is None :
-            return
-        
-        group = self.config_path+"/"+self._get_identifier(self._function)
-        
-        if not wx.ConfigBase_Get().HasGroup(group) :
-            return
-        
-        old_path = wx.ConfigBase_Get().GetPath()
-        wx.ConfigBase_Get().SetPath(group)
-        
-        has_next, entry, cookie = wx.ConfigBase_Get().GetFirstEntry()
-        while has_next :
-            value = wx.ConfigBase_Get().Read(entry)
-            try :
-                value = cPickle.loads(str(value))
-            except :
-                # Could not unpickle, don't restore
-                pass
-            else :
-                try :
-                    self._controls[entry].value = value
-                except :
-                    # Could not restore, ignore exception
-                    pass
-            has_next, entry, cookie = wx.ConfigBase_Get().GetNextEntry(cookie)
-        
-        wx.ConfigBase_Get().SetPath(old_path)
-
-    def _save_parameters(self):
-        """ Save the current values of the paramters (when possible).
-        """
-        
-        if self.config_path is None :
-            return
-        
-        group = self.config_path+"/"+self._get_identifier(self._function)
-        
-        old_path = wx.ConfigBase_Get().GetPath()
-        wx.ConfigBase_Get().SetPath(group)
-        
-        # Save the current parameters
-        for parameter in self._parameters :
-            value = self._controls[parameter["name"]].value
-            try :
-                value = cPickle.dumps(value)
-            except :
-                # Could not pickle, don't save it
-                continue
-            
-            wx.ConfigBase_Get().Write(parameter["name"], value)
-
-        wx.ConfigBase_Get().SetPath(old_path)
+#    def _get_identifier(self, function):
+#        """ Return the unique identifier for given function (used in 
+#            _load_parameters and _save_parameters).
+#        """
+#        
+#        identifier = "/".join([
+#            sys.modules[function.__module__].__file__, function.__name__])
+#        identifier = hashlib.md5(identifier).hexdigest()
+#        
+#        return identifier
+#
+#    def _load_parameters(self):
+#        """ Load the previously-saved values of the parameters (when possible).
+#        """
+#        
+#        if self.config_path is None :
+#            return
+#        
+#        group = self.config_path+"/"+self._get_identifier(self._function)
+#        
+#        if not wx.ConfigBase_Get().HasGroup(group) :
+#            return
+#        
+#        old_path = wx.ConfigBase_Get().GetPath()
+#        wx.ConfigBase_Get().SetPath(group)
+#        
+#        has_next, entry, cookie = wx.ConfigBase_Get().GetFirstEntry()
+#        while has_next :
+#            value = wx.ConfigBase_Get().Read(entry)
+#            try :
+#                value = cPickle.loads(str(value))
+#            except :
+#                # Could not unpickle, don't restore
+#                pass
+#            else :
+#                try :
+#                    self._controls[entry].value = value
+#                except :
+#                    # Could not restore, ignore exception
+#                    pass
+#            has_next, entry, cookie = wx.ConfigBase_Get().GetNextEntry(cookie)
+#        
+#        wx.ConfigBase_Get().SetPath(old_path)
+#
+#    def _save_parameters(self):
+#        """ Save the current values of the paramters (when possible).
+#        """
+#        
+#        if self.config_path is None :
+#            return
+#        
+#        group = self.config_path+"/"+self._get_identifier(self._function)
+#        
+#        old_path = wx.ConfigBase_Get().GetPath()
+#        wx.ConfigBase_Get().SetPath(group)
+#        
+#        # Save the current parameters
+#        for parameter in self._parameters :
+#            value = self._controls[parameter["name"]].value
+#            try :
+#                value = cPickle.dumps(value)
+#            except :
+#                # Could not pickle, don't save it
+#                continue
+#            
+#            wx.ConfigBase_Get().Write(parameter["name"], value)
+#
+#        wx.ConfigBase_Get().SetPath(old_path)
