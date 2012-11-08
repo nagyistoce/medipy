3b0cd1370261776878de4509949cc792cc9c0146
Julien Lamy <lamy@unistra.fr>
2012-11-08 16:54:09 +0100

    plugins/diffusion : remove wrapper for SymmetricSpectralAnalysisImageFilter

--- plugins/diffusion/__init__.py
+++ plugins/diffusion/__init__.py
@@ -4,7 +4,6 @@ import medipy.itk
 import estimation
 import utils
 
-medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "SymmetricSpectralAnalysisImageFilter")
 medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "SecondOrderSymmetricTensorReconstructionFilter")
 medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MeanDiffusivityImageFilter")
 medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "FractionalAnisotropyImageFilter")
