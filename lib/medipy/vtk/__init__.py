e8a10477bb044eced956be0bce7efefde1ebda51
Julien Lamy <lamy@unistra.fr>
2012-11-09 14:16:31 +0100

    lib/vtk/bridge : remove get_vtk_array_type and build_vtk_image

--- lib/medipy/vtk/__init__.py
+++ lib/medipy/vtk/__init__.py
@@ -9,7 +9,6 @@
 from vtk_addons import (
     vtkColorTransferFunctionWithAlpha, vtkEnhancedLookupTable, vtkMisc,
     vtkOrientationAnnotation)
-from bridge import (get_vtk_array_type, build_vtk_image, array_to_vtk_image, 
-                    vtk_image_to_array, vtk_image_to_medipy_image,
-                    build_vtk_colormap, build_lookup_table, 
-                    build_color_transfer_function_with_alpha)
+from bridge import (array_to_vtk_image, vtk_image_to_array, 
+                    vtk_image_to_medipy_image, build_vtk_colormap, 
+                    build_lookup_table, build_color_transfer_function_with_alpha)
