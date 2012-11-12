e2f77da5707df6ebe9a1cd03f048eb06a4db3b66
Antoine Grigis <grigis@unistra.fr>
2012-11-12 12:26:20 +0100

    plugins/diffusion/tensor : compute white matter fibers skeleton

--- plugins/diffusion/fiber_clustering.pyx
+++ plugins/diffusion/fiber_clustering.pyx
@@ -7,7 +7,8 @@ import numpy as np
 cimport numpy as cnp
 
 cdef extern from "math.h" nogil:
-    float sqrt(float x)  
+    float sqrt(float x) 
+    float fabs(float x) 
       
 cdef extern from "stdlib.h" nogil:
     ctypedef unsigned long size_t
@@ -22,6 +23,7 @@ cdef inline float* asfp(cnp.ndarray pt):
 
 # float 32 dtype for casting
 cdef cnp.dtype f32_dt = np.dtype(np.float32)
+cdef cnp.float32_t inf = np.inf
 
 
 def local_skeleton_clustering(tracks, d_thr=10):
@@ -186,6 +188,429 @@ def length(xyz, along=False):
     if along:
         return np.cumsum(dists)
     return np.sum(dists)
+
+
+
+
+
+
+
+
+
+
+
+
+
+def most_similar_track_mam(tracks,metric='avg'):    
+    ''' Find the most similar track in a bundle 
+    using distances calculated from Zhang et. al 2008. 
+    
+    Parameters
+    ----------
+    tracks : sequence 
+       of tracks as arrays, shape (N1,3) .. (Nm,3)
+    metric : str
+       'avg', 'min', 'max'
+        
+    Returns
+    -------
+    si : int
+       index of the most similar track in tracks. This can be used as a
+       reference track for a bundle.
+    s : array, shape (len(tracks),)
+        similarities between tracks[si] and the rest of the tracks in
+        the bundle
+    
+    Notes
+    -----
+    A vague description of this function is given below:
+    
+    for (i,j) in tracks_combinations_of_2:
+
+        calculate the mean_closest_distance from i to j  (mcd_i)  
+        calculate the mean_closest_distance from j to i  (mcd_j)
+        
+        if 'avg':
+            s holds the average similarities
+        if 'min':
+            s holds the minimum similarities
+        if 'max':
+            s holds the maximum similarities
+        
+    si holds the index of the track with min {avg,min,max} average metric
+    '''
+    cdef:
+        size_t i, j, lent
+        int metric_type
+    if metric=='avg':
+        metric_type = 0
+    elif metric == 'min':
+        metric_type = 1
+    elif metric == 'max':
+        metric_type = 2
+    else:
+        raise ValueError('Metric should be one of avg, min, max')
+    # preprocess tracks
+    cdef:
+        size_t longest_track_len = 0, track_len
+        cnp.ndarray[object, ndim=1] tracks32
+    lent = len(tracks)
+    tracks32 = np.zeros((lent,), dtype=object)
+    # process tracks to predictable memory layout, find longest track
+    for i in range(lent):
+        tracks32[i] = np.ascontiguousarray(tracks[i], dtype=f32_dt)
+        track_len = tracks32[i].shape[0]
+        if track_len > longest_track_len:
+            longest_track_len = track_len
+    # buffer for distances of found track to other tracks
+    cdef:
+        cnp.ndarray[cnp.double_t, ndim=1] track2others
+    track2others = np.zeros((lent,), dtype=np.double)
+    # use this buffer also for working space containing summed distances
+    # of candidate track to all other tracks
+    cdef cnp.double_t *sum_track2others = <cnp.double_t *>track2others.data
+    # preallocate buffer array for track distance calculations
+    cdef:
+        cnp.ndarray [cnp.float32_t, ndim=1] distances_buffer
+        cnp.float32_t *t1_ptr, *t2_ptr, *min_buffer, distance
+    distances_buffer = np.zeros((longest_track_len*2,), dtype=np.float32)
+    min_buffer = <cnp.float32_t *> distances_buffer.data
+    # cycle over tracks
+    cdef:
+        cnp.ndarray [cnp.float32_t, ndim=2] t1, t2
+        size_t t1_len, t2_len
+    for i from 0 <= i < lent-1:
+        t1 = tracks32[i]
+        t1_len = t1.shape[0]
+        t1_ptr = <cnp.float32_t *>t1.data
+        for j from i+1 <= j < lent:
+            t2 = tracks32[j]
+            t2_len = t2.shape[0]
+            t2_ptr = <cnp.float32_t *>t2.data
+            distance = czhang(t1_len, t1_ptr, t2_len, t2_ptr, min_buffer, metric_type)
+            # get metric
+            sum_track2others[i]+=distance
+            sum_track2others[j]+=distance
+    # find track with smallest summed metric with other tracks
+    cdef double mn = sum_track2others[0]
+    cdef size_t si = 0
+    for i in range(lent):
+        if sum_track2others[i] < mn:
+            si = i
+            mn = sum_track2others[i]
+    # recalculate distance of this track from the others
+    t1 = tracks32[si]
+    t1_len = t1.shape[0]
+    t1_ptr = <cnp.float32_t *>t1.data
+    for j from 0 <= j < lent:
+        t2 = tracks32[j]
+        t2_len = t2.shape[0]
+        t2_ptr = <cnp.float32_t *>t2.data
+        track2others[j] = czhang(t1_len, t1_ptr, t2_len, t2_ptr, min_buffer, metric_type)
+    return si, track2others
+	
+	
+	
+@cython.cdivision(True)
+cdef inline cnp.float32_t czhang(size_t t1_len,
+                                 cnp.float32_t *track1_ptr,
+                                 size_t t2_len,
+                                 cnp.float32_t *track2_ptr,
+                                 cnp.float32_t *min_buffer,
+                                 int metric_type) nogil:
+    ''' Note ``nogil`` - no python calls allowed in this function '''
+    cdef:
+        cnp.float32_t *min_t2t1, *min_t1t2
+    min_t2t1 = min_buffer
+    min_t1t2 = min_buffer + t2_len
+    min_distances(t1_len, track1_ptr,
+                  t2_len, track2_ptr,
+                  min_t2t1,
+                  min_t1t2)
+    cdef:
+        size_t t1_pi, t2_pi
+        cnp.float32_t mean_t2t1 = 0, mean_t1t2 = 0, dist_val
+    for t1_pi from 0<= t1_pi < t1_len:
+        mean_t1t2+=min_t1t2[t1_pi]
+    mean_t1t2=mean_t1t2/t1_len
+    for t2_pi from 0<= t2_pi < t2_len:
+        mean_t2t1+=min_t2t1[t2_pi]
+    mean_t2t1=mean_t2t1/t2_len
+    if metric_type == 0:                
+        dist_val=(mean_t2t1+mean_t1t2)/2.0
+    elif metric_type == 1:        
+        if mean_t2t1 < mean_t1t2:
+            dist_val=mean_t2t1
+        else:
+            dist_val=mean_t1t2
+    elif metric_type == 2:                
+        if mean_t2t1 > mean_t1t2:
+            dist_val=mean_t2t1
+        else:
+            dist_val=mean_t1t2                    
+    return dist_val
+
+@cython.cdivision(True)
+cdef inline void min_distances(size_t t1_len,
+                               cnp.float32_t *track1_ptr,
+                               size_t t2_len,
+                               cnp.float32_t *track2_ptr,
+                               cnp.float32_t *min_t2t1,
+                               cnp.float32_t *min_t1t2) nogil:
+    cdef:
+        cnp.float32_t *t1_pt, *t2_pt, d0, d1, d2
+        cnp.float32_t delta2
+        int t1_pi, t2_pi
+    for t2_pi from 0<= t2_pi < t2_len:
+        min_t2t1[t2_pi] = inf
+    for t1_pi from 0<= t1_pi < t1_len:
+        min_t1t2[t1_pi] = inf
+    # pointer to current point in track 1
+    t1_pt = track1_ptr
+    # calculate min squared distance between each point in the two
+    # lines.  Squared distance to delay doing the sqrt until after this
+    # speed-critical loop
+    for t1_pi from 0<= t1_pi < t1_len:
+        # pointer to current point in track 2
+        t2_pt = track2_ptr
+        for t2_pi from 0<= t2_pi < t2_len:
+            d0 = t1_pt[0] - t2_pt[0]
+            d1 = t1_pt[1] - t2_pt[1]
+            d2 = t1_pt[2] - t2_pt[2]
+            delta2 = d0*d0 + d1*d1 + d2*d2
+            if delta2 < min_t2t1[t2_pi]:
+                min_t2t1[t2_pi]=delta2
+            if delta2 < min_t1t2[t1_pi]:
+                min_t1t2[t1_pi]=delta2
+            t2_pt += 3 # to next point in track 2
+        t1_pt += 3 # to next point in track 1
+    # sqrt to get Euclidean distance from squared distance
+    for t1_pi from 0<= t1_pi < t1_len:
+        min_t1t2[t1_pi]=sqrt(min_t1t2[t1_pi])
+    for t2_pi from 0<= t2_pi < t2_len:
+        min_t2t1[t2_pi]=sqrt(min_t2t1[t2_pi])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def cut_plane(tracks,ref):
+    ''' Extract divergence vectors and points of intersection 
+    between planes normal to the reference fiber and other tracks
+    
+    Parameters
+    ----------
+    tracks: sequence
+        of tracks as arrays, shape (N1,3) .. (Nm,3)
+    ref: array, shape (N,3)
+        reference track
+        
+    Returns
+    -------
+    hits: sequence
+       list of points and rcds (radial coefficient of divergence)``
+    
+    Examples
+    --------
+    >>> from tools import cut_plane
+    >>> refx = np.array([[0,0,0],[1,0,0],[2,0,0],[3,0,0]],dtype='float32')
+    >>> bundlex = [np.array([[0.5,1,0],[1.5,2,0],[2.5,3,0]],dtype='float32')]
+    >>> cut_plane(bundlex,refx)
+    [array([[ 1.        ,  1.5       ,  0.        ,  0.70710683, 0,]], dtype=float32),
+     array([[ 2.        ,  2.5       ,  0.        ,  0.70710677, 0.]], dtype=float32)]
+        
+    The orthogonality relationship
+    np.inner(hits[p][q][0:3]-ref[p+1],ref[p+2]-ref[r][p+1])
+    will hold throughout for every point q in the hits plane
+    at point (p+1) on the reference track.
+    '''
+    cdef:
+        size_t n_hits, hit_no, max_hit_len
+        float alpha,beta,lrq,rcd,lhp,ld
+        cnp.ndarray[cnp.float32_t, ndim=2] ref32
+        cnp.ndarray[cnp.float32_t, ndim=2] track
+        object hits
+        cnp.ndarray[cnp.float32_t, ndim=1] one_hit
+        float *hit_ptr
+        cnp.ndarray[cnp.float32_t, ndim=2] hit_arr
+        object Hit=[]
+    # make reference fiber usable type
+    ref32 = np.ascontiguousarray(ref, f32_dt)
+    # convert all the tracks to something we can work with.  Get track
+    # lengths
+    cdef:
+        size_t N_tracks=len(tracks)
+        cnp.ndarray[cnp.uint64_t, ndim=1] track_lengths
+        size_t t_no, N_track
+    cdef object tracks32 = []
+    track_lengths = np.empty((N_tracks,), dtype=np.uint64)
+    for t_no in range(N_tracks):
+        track = np.ascontiguousarray(tracks[t_no], f32_dt)
+        track_lengths[t_no] = track.shape[0]
+        tracks32.append(track)
+    # set up loop across reference fiber points
+    cdef:
+        size_t N_ref = ref32.shape[0]
+        size_t p_no, q_no
+        float *this_ref_p, *next_ref_p, *this_trk_p, *next_trk_p
+        float along[3], normal[3]
+        float qMp[3], rMp[3], rMq[3], pMq[3]
+        float hit[3], hitMp[3], *delta
+    # List used for storage of hits.  We will fill this with lots of
+    # small numpy arrays, and reuse them over the reference track point
+    # loops.
+    max_hit_len = 0
+    hits = []
+    # for every point along the reference track
+    next_ref_p = asfp(ref32[0])
+    for p_no in range(N_ref-1):
+        # extract point to point vector into `along`
+        this_ref_p = next_ref_p
+        next_ref_p = asfp(ref32[p_no+1])
+        csub_3vecs(next_ref_p, this_ref_p, along)
+        # normalize
+        cnormalized_3vec(along, normal)
+        # initialize index for hits
+        hit_no = 0
+        # for every track
+        for t_no in range(N_tracks):
+            track=tracks32[t_no]
+            N_track = track_lengths[t_no]
+            # for every point on the track
+            next_trk_p = asfp(track[0])
+            for q_no in range(N_track-1):
+                # p = ref32[p_no]
+                # q = track[q_no]
+                # r = track[q_no+1]
+                # float* versions of above: p == this_ref_p
+                this_trk_p = next_trk_p # q
+                next_trk_p = asfp(track[q_no+1]) # r
+                #if np.inner(normal,q-p)*np.inner(normal,r-p) <= 0:
+                csub_3vecs(this_trk_p, this_ref_p, qMp) # q-p
+                csub_3vecs(next_trk_p, this_ref_p, rMp) # r-p
+                if (cinner_3vecs(normal, qMp) * cinner_3vecs(normal, rMp)) <=0:
+                    #if np.inner((r-q),normal) != 0:
+                    csub_3vecs(next_trk_p, this_trk_p, rMq)
+                    beta = cinner_3vecs(rMq, normal)
+                    if beta !=0:
+                        #alpha = np.inner((p-q),normal)/np.inner((r-q),normal)
+                        csub_3vecs(this_ref_p, this_trk_p, pMq)
+                        alpha = (cinner_3vecs(pMq, normal) /
+                                  cinner_3vecs(rMq, normal))
+                        if alpha < 1:
+                            # hit = q+alpha*(r-q)
+                            hit[0] = this_trk_p[0]+alpha*rMq[0]
+                            hit[1] = this_trk_p[1]+alpha*rMq[1]
+                            hit[2] = this_trk_p[2]+alpha*rMq[2]
+                            # h-p
+                            csub_3vecs(hit, this_ref_p, hitMp)
+                            # |h-p|
+                            lhp = cnorm_3vec(hitMp)
+                            delta = rMq # just renaming
+                            # |r-q| == |delta|
+                            ld = cnorm_3vec(delta)
+                            ''' # Summary of stuff in comments
+                            # divergence =((r-q)-inner(r-q,normal)*normal)/|r-q|
+                            div[0] = (rMq[0]-beta*normal[0]) / ld
+                            div[1] = (rMq[1]-beta*normal[1]) / ld
+                            div[2] = (rMq[2]-beta*normal[2]) / ld
+                            # radial coefficient of divergence d.(h-p)/|h-p|
+                            '''
+                            # radial divergence
+                            # np.inner(delta, (hit-p)) / (ld * lhp)
+                            if lhp > 0:
+                                rcd = fabs(cinner_3vecs(delta, hitMp)
+                                           / (ld*lhp))
+                            else:
+                                rcd=0
+                            # hit data into array
+                            if hit_no >= max_hit_len:
+                                one_hit = np.empty((5,), dtype=f32_dt)
+                                hits.append(one_hit)
+                            else:
+                                one_hit = hits[hit_no]
+                            hit_ptr = <float *>one_hit.data
+                            hit_ptr[0] = hit[0]
+                            hit_ptr[1] = hit[1]
+                            hit_ptr[2] = hit[2]
+                            hit_ptr[3] = rcd
+                            hit_ptr[4] = t_no
+                            hit_no += 1
+        # convert hits list to hits array
+        n_hits = hit_no
+        if n_hits > max_hit_len:
+            max_hit_len = n_hits
+        hit_arr = np.empty((n_hits,5), dtype=f32_dt)
+        for hit_no in range(n_hits):
+            hit_arr[hit_no] = hits[hit_no]
+        Hit.append(hit_arr)
+        #Div.append(divs[1:])
+    return Hit[1:] 
+
+cdef inline float cnorm_3vec(float *vec):
+    ''' Calculate Euclidean norm of input vector
+
+    Parameters
+    ----------
+    vec : float *
+       length 3 float vector
+
+    Returns
+    -------
+    norm : float
+       Euclidean norm
+    '''
+    cdef float v0, v1, v2
+    v0 = vec[0]
+    v1 = vec[1]
+    v2 = vec[2]
+    return sqrt(v0 * v0 + v1*v1 + v2*v2) 
+
+cdef inline float cinner_3vecs(float *vec1, float *vec2) nogil:
+    cdef int i
+    cdef float ip = 0
+    for i from 0<=i<3:
+        ip += vec1[i]*vec2[i]
+    return ip
+
+cdef inline void csub_3vecs(float *vec1, float *vec2, float *vec_out) nogil:
+    cdef int i
+    for i from 0<=i<3:
+        vec_out[i] = vec1[i]-vec2[i]
+
+cdef inline void cnormalized_3vec(float *vec_in, float *vec_out):
+    ''' Calculate and fill normalized 3D vector 
+
+    Parameters
+    ----------
+    vec_in : float *
+       Length 3 vector to normalize
+    vec_out : float *
+       Memory into which to write normalized length 3 vector
+
+    Returns
+    -------
+    void
+    '''
+    cdef float norm = cnorm_3vec(vec_in)
+    cdef int i
+    norm=norm+(norm==0)
+    for i in range(3):
+        vec_out[i] = vec_in[i] / norm
 
 
 
--- plugins/diffusion/tractography.py
+++ plugins/diffusion/tractography.py
@@ -16,37 +16,138 @@ import medipy.itk
 import fiber_clustering
 from utils import length, generate_image_sampling
 
-def streamline_tractography(model, seeds=None, step=1.0, min_fa=0.2, 
-                            max_angle=numpy.pi/3.0, min_length=50.0, 
-                            propagation_type="Euler", clustering_type="None",
-                            clustering_threshold=20):
+def streamline_tractography_gui(model,*args,**kwargs) :
     """ Streamline 2nd order tensor tractography 
- 
+
     <gui>
         <item name="model" type="Image" label="Input"/>
         <item name="step" type="Float" initializer="1.0" label="Propagation step"/>
-        <item name="min_fa" type="Float" initializer="0.2" label="FA threshold"/>
-        <item name="max_angle" type="Float" initializer="1.04" label="Angle threshold"/>
-        <item name="min_length" type="Float" initializer="50.0" label="Length threshold"/>
-        <item name="propagation_type" type="Enum" 
-            initializer="('Euler', 'Runge Kutta 4')" 
-            label="Choose propagation order"/>
-        <item name="clustering_type" type="Enum" initializer="('None', 'Fast')" 
-              label="Choose the clustering strategy"/>
-        <item name="clustering_threshold" type="Float" initializer="20.0" 
-              label="Threshold in clustering"/>
+        <item name="thr_fa" type="Float" initializer="0.2" label="FA threshold"/>
+        <item name="thr_angle" type="Float" initializer="1.04" label="Angle threshold"/>
+        <item name="thr_length" type="Float" initializer="50.0" label="Length threshold"/>
+        <item name="thr_clustering" type="Float" initializer="20.0" label="Threshold in clustering"/>
+        <item name="propagation_type" type="Enum" initializer="('Euler', 'Runge Kutta 4')" label="Choose propagation order"/>
+        <item name="clustering_type" type="Enum" initializer="('None', 'Fast')" label="Choose the clustering strategy"/>
         <item name="output" type="Object3D" role="return" label="Output"/>
     </gui>
     """
-    
-    if seeds is None :
-        seeds = generate_image_sampling(model,step=2.0*model.spacing)
+
+    step = kwargs['step']
+    thr_fa = kwargs['thr_fa']
+    thr_angle = kwargs['thr_angle']
+    thr_length = kwargs['thr_length']
+    propagation_type = kwargs['propagation_type']
+    clustering_type = kwargs['clustering_type']
+    thr_clustering = kwargs['thr_clustering']
+
+    T,C = streamline_tractography(model,step,thr_fa,thr_angle,thr_length,propagation_type,clustering_type,thr_clustering)
+    vtk_polydata = fiber_polydata(T,C)
+    output = Object3D(vtk_polydata,"Streamline Tractography")
+
+    return output
+
+def skeleton_gui(model,*args,**kwargs) :
+    """ Skeleton of the white matter fibersStreamline 2nd order tensor tractography 
+
+    <gui>
+        <item name="model" type="Image" label="Input"/>
+        <item name="step" type="Float" initializer="1.0" label="Propagation step"/>
+        <item name="thr_fa" type="Float" initializer="0.2" label="FA threshold"/>
+        <item name="thr_angle" type="Float" initializer="1.04" label="Angle threshold"/>
+        <item name="thr_length" type="Float" initializer="50.0" label="Length threshold"/>
+        <item name="thr_clustering" type="Float" initializer="20.0" label="Threshold in clustering"/>
+        <item name="propagation_type" type="Enum" initializer="('Euler', 'Runge Kutta 4')" label="Choose propagation order"/>
+        <item name="clustering_type" type="Enum" initializer="('Fast',)" label="Choose the clustering strategy"/>
+        <item name="output" type="Object3D" role="return" label="Output"/>
+    </gui>
+    """
+
+    step = kwargs['step']
+    thr_fa = kwargs['thr_fa']
+    thr_angle = kwargs['thr_angle']
+    thr_length = kwargs['thr_length']
+    propagation_type = kwargs['propagation_type']
+    clustering_type = kwargs['clustering_type']
+    thr_clustering = kwargs['thr_clustering']
+
+    T,C = streamline_tractography(model,step,thr_fa,thr_angle,thr_length,propagation_type,clustering_type,thr_clustering)
+    skeleton(T,C)
+    vtk_polydata = fiber_polydata(T,C)
+    output = Object3D(vtk_polydata,"Fiber Skeleton")
+
+    return output
+
+def skeleton(T,C) :
+    """ Skeleton of the white matter fibersStreamline 2nd order tensor tractography 
+    """
+
+    for key in C.keys():
+        N = C[key]['N']
+        bundle_indices = C[key]['indices']
+        B = []
+        for indice in bundle_indices:
+            B.append(T[indice])
+        s,si = medipy.diffusion.fiber_clustering.most_similar_track_mam(B,metric='avg') 
+        C[key]['skel'] = bundle_indices[s]  
+
+def fiber_polydata(T,C) : 
+    """ Construct the vtk polydata 
+    """
+
+    clusters_skel = np.ones((len(T),))
+    clusters = np.ones((len(T),))
+    nb_clusters = len(C.keys())
+    flag = 'skel' in C[C.keys()[0]].keys()
+    for cnt,c in enumerate(C.keys()) :
+        indices = C[c]['indices']
+        for i in indices :
+            clusters[i] = float(cnt)/ float(nb_clusters)  
+    if flag :
+        for cnt,c in enumerate(C.keys()) :
+            indice = C[c]['skel']
+            clusters_skel[indice] = 0.0
+
+    fusion = vtk.vtkAppendPolyData()
+
+    for cluster,cluster_skel,fiber in zip(clusters,clusters_skel,T) :
+        nb_points = fiber.shape[0]
+        scalars = vtk.vtkFloatArray()
+        scalars.SetName("cluster")
+        scalars_skel = vtk.vtkFloatArray()
+        scalars_skel.SetName("skeleton")
+        points= vtk.vtkPoints()
+        line = vtk.vtkCellArray()
+        line.InsertNextCell(nb_points)
+
+        for i in range(nb_points):
+            z,y,x = fiber[i]
+            points.InsertPoint(i,(x,y,z))
+            scalars.InsertTuple1(i,cluster)
+            scalars_skel.InsertTuple1(i,cluster_skel)
+            line.InsertCellPoint(i)
+
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(points)
+        if flag :
+            polydata.GetPointData().AddArray(scalars)
+            polydata.GetPointData().AddArray(scalars_skel)
+        else :
+            polydata.GetPointData().SetScalars(scalars)  
+        polydata.SetLines(line) 
+
+        fusion.AddInput(polydata)
+  
+    fusion.Update()
+    return fusion.GetOutput()
+
+def streamline_tractography(model,step,thr_fa,thr_angle,thr_length,propagation_type,clustering_type,thr_clustering) :
+    """ Streamline 2nd order tensor tractography 
+    """
+
+    seeds = generate_image_sampling(model,step=2.0*model.spacing)
     ndim = len(model.shape)
 
-    VectorImage = itk.VectorImage[medipy.itk.dtype_to_itk[model.dtype.type], model.ndim]
-    Image = itk.Image[medipy.itk.dtype_to_itk[model.dtype.type], model.ndim]
-
-    tractography_filter = itk.StreamlineTractographyAlgorithm[VectorImage, Image].New()
+    tractography_filter = itk.StreamlineTractographyAlgorithm[itk.VectorImage[itk.F,ndim], itk.Image[itk.F,ndim]].New()
 
     itk_model = medipy.itk.medipy_image_to_itk_image(model, False)
     tractography_filter.SetInputModel(itk_model)
@@ -57,8 +158,8 @@ def streamline_tractography(model, seeds=None, step=1.0, min_fa=0.2,
         tractography_filter.SetUseRungeKuttaOrder4(False)
     else :
         tractography_filter.SetUseRungeKuttaOrder4(True)
-    tractography_filter.SetThresholdAngle(max_angle)
-    tractography_filter.SetThresholdFA(min_fa)
+    tractography_filter.SetThresholdAngle(thr_angle)
+    tractography_filter.SetThresholdFA(thr_fa)
     tractography_filter.Update()
 
     nb_fiber = tractography_filter.GetNumberOfFibers()
@@ -75,36 +176,64 @@ def streamline_tractography(model, seeds=None, step=1.0, min_fa=0.2,
     if clustering_type=='Fast' :
         C = medipy.diffusion.fiber_clustering.local_skeleton_clustering(final_fibers, d_thr=thr_clustering)
 
-    clusters = np.ones((len(final_fibers),))
-    nb_clusters = len(C.keys())
-    for cnt,c in enumerate(C.keys()) :
-        indices = C[c]['indices']
-        for i in indices :
-            clusters[i] = float(cnt)/ float(nb_clusters)    
+    return final_fibers,C
+
+
+def streamline_tractography_(model,seeds=None,step=1.0,thr_fa=0.2,thr_angle=np.pi,rk4=False):
+    """ Streamline 2nd order tensor tractography """
+
+    if seeds==None :
+            seeds = generate_image_sampling(model,step=(1,1,1))
+    print len(seeds)
+
+    tractography_filter = itk.StreamlineTractographyAlgorithm[itk.VectorImage[itk.F,3], itk.Image[itk.F,3]].New()
+
+    itk_model = medipy.itk.medipy_image_to_itk_image(model, False)
+    tractography_filter.SetInputModel(itk_model)
+    for seed in seeds:
+        tractography_filter.AppendSeed(seed)
+    tractography_filter.SetStepSize(step)
+    tractography_filter.SetUseRungeKuttaOrder4(rk4)
+    tractography_filter.SetThresholdAngle(thr_angle)
+    tractography_filter.SetThresholdFA(thr_fa)
+    tractography_filter.Update()
+
+    nb_fiber = tractography_filter.GetNumberOfFibers()
+    fibers = []
+    for i in range(nb_fiber) :
+        fibers.append(tractography_filter.GetOutputFiberAsPyArray(i))
+    print [ fiber.shape for fiber in fibers]
+
+    save_fibers(fibers,fout="/home/grigis/Bureau/fibers_test.vtk",step=step,thr_length=50.0)
+
+
+def save_fibers(fibers,fout,step=None,thr_length=0.0,ensemble=1) :
 
     fusion = vtk.vtkAppendPolyData()
 
-    for cluster,fiber in zip(clusters,final_fibers) :
-        nb_points = fiber.shape[0]
-        scalars = vtk.vtkFloatArray()
-        scalars.SetName("clusters")
-        points= vtk.vtkPoints()
-        line = vtk.vtkCellArray()
-        line.InsertNextCell(nb_points)
+    for fiber in fibers :
 
-        for i in range(nb_points):
-            z,y,x = fiber[i]
-            points.InsertPoint(i,(x,y,z))
-            scalars.InsertTuple1(i,cluster)
-            line.InsertCellPoint(i)
+        if length(fiber,step)>=thr_length :
+            nb_points = fiber.shape[0]
+            scalars = vtk.vtkFloatArray()
+            points= vtk.vtkPoints()
+            line = vtk.vtkCellArray()
+            line.InsertNextCell(nb_points)
 
-        polydata = vtk.vtkPolyData()
-        polydata.SetPoints(points)
-        polydata.GetPointData().SetScalars(scalars)
-        polydata.SetLines(line) 
+            for i in range(nb_points):
+                z,y,x = fiber[i]
+                points.InsertPoint(i,(x,y,z))
+                scalars.InsertTuple1(i,ensemble)
+                line.InsertCellPoint(i)
 
-        fusion.AddInput(polydata)
-   
-    fusion.Update()
-    output = Object3D(fusion.GetOutput(),"Streamline Tractography")
-    return output
+            polydata = vtk.vtkPolyData()
+            polydata.SetPoints(points)
+            polydata.GetPointData().SetScalars(scalars)
+            polydata.SetLines(line) 
+
+            fusion.AddInput(polydata)
+
+    writer = vtk.vtkPolyDataWriter()
+    writer.SetFileName(fout)
+    writer.SetInput(fusion.GetOutput())
+    writer.Update()
