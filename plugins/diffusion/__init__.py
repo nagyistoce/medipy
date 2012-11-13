416cb7144b950fa0c87a2255df0aaaa3285ed91c
Julien Lamy <lamy@unistra.fr>
2012-11-13 12:35:46 +0100

    plugins/diffusion : add Tensor2 reader/writer to SConstruct

--- plugins/diffusion/__init__.py
+++ plugins/diffusion/__init__.py
@@ -1,10 +1,8 @@
 import os.path
 import medipy.itk
 
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MediPyDiffusion")
+
 import estimation
 from spectral_analysis import spectral_analysis
 import utils
-
-medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "SecondOrderSymmetricTensorReconstructionFilter")
-medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MeanDiffusivityImageFilter")
-medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "FractionalAnisotropyImageFilter")
