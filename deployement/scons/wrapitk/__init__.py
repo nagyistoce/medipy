3f4acd76e02627a901d562505b8aad307f59fb2a
Julien Lamy <lamy@unistra.fr>
2012-11-13 12:26:28 +0100

    deployement/scons : fix dependancies handling in module config file

--- deployement/scons/wrapitk/__init__.py
+++ deployement/scons/wrapitk/__init__.py
@@ -5,5 +5,5 @@ import module_builders
 import module_wrapper
 import utils
 
-from config import get_scalar_types, get_dimensions
+from config import get_real_types, get_int_types, get_scalar_types, get_dimensions
 from utils import Instantiation, get_instantiations
