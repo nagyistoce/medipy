11f6ebeeafc491564e57eca823a5162bb8c94333
Antoine Grigis <grigis@unistra.fr>
2012-11-14 12:38:49 +0100

    plugins/diffusion/fiber_statistics : local and global statistics along bundles

--- plugins/diffusion/fiber_statistics.py
+++ plugins/diffusion/fiber_statistics.py
@@ -7,10 +7,284 @@
 # for details.                                                      
 ##########################################################################
 
-
+import medipy.base
 import numpy as np
 import medipy.diffusion.fiber_clustering
 import sys
+
+
+def compute_statistic_section(dt1,dt2,C_hits_1,C_hits_2,nb_plane=1,nbClust=27):
+    ''' Statistic in bundle cross sections
+    '''
+
+    spm = []
+    dim1 = dt1.shape
+    dim2 = dt2.shape
+    spacing1 = dt1.spacing
+    spacing2 = dt2.spacing
+
+    for c1,c2 in zip(C_hits_1.keys(),C_hits_2.keys()):
+        sys.stdout.write("\riteration= %d, %d / %d, %d" %(c1,c2,len(C_hits_1.keys()),len(C_hits_2.keys())))
+        sys.stdout.flush()
+
+        population1 = C_hits_1[c1]['indices']
+        population2 = C_hits_2[c2]['indices']
+        skel1 = C_hits_1[c1]['skel']
+        skel2 = C_hits_2[c2]['skel']
+
+        T = []
+        shift_plane = nb_plane-1
+        if len(population1)==len(population2):
+            l_pop = len(population1)
+        else:
+            medipy.base.Exception("Can only compare two populations of the same size")    
+
+        # impossible to get neighborhood
+        for cnt1 in range(shift_plane):
+            T.append( (0.,3.,1.) )
+
+        # iteration
+        for cnt1 in range(shift_plane,l_pop-shift_plane):
+
+            # get neighborhood
+            points1 = np.zeros((0,3))
+            weights1 = np.zeros((0,))
+            points2 = np.zeros((0,3))
+            weights2 = np.zeros((0,))
+            for cnt2 in range(cnt1-shift_plane,cnt1+nb_plane):
+	            pop1_tmp = []
+	            for point in population1[cnt2]:
+		            k = (point<dim1).all()
+		            if k:
+			            pop1_tmp.append( point )
+	            if pop1_tmp!=[]:
+		            pop1_tmp = np.asarray( pop1_tmp )
+		            points1 = np.concatenate( (points1,pop1_tmp) )
+		            weights1 = np.concatenate( (weights1,np.repeat(np.abs(cnt2-cnt1),pop1_tmp.shape[0])) )
+
+	            pop2_tmp = []
+	            for point in population2[cnt2]:
+		            k = (point<dim2).all()
+		            if k:
+			            pop2_tmp.append( point )
+	            if pop2_tmp!=[]:
+		            pop2_tmp = np.asarray( pop2_tmp )
+		            points2 = np.concatenate( (points2,pop2_tmp) )
+		            weights2 = np.concatenate( (weights2,np.repeat(np.abs(cnt2-cnt1),pop2_tmp.shape[0])) )
+
+            # reduce data set
+            if points1.shape[0]>0:
+	            center1 = skel1[cnt1]
+	            points1 = cluster_voxel_2(points1,center1,weights1,spacing1)
+            if points2.shape[0]>0:
+	            center2 = skel2[cnt1]
+	            points2 = cluster_voxel_2(points2,center2,weights2,spacing2)
+
+            # statistic
+            N1 = float(points1.shape[0])
+            N2 = float(points2.shape[0])
+            if N1>=nbClust and N2>=nbClust:
+	            N1 = float(nbClust)
+	            points1 = points1[:nbClust]
+	            N2 = float(nbClust)
+	            points2 = points2[:nbClust]
+            else:
+	            N1 = 1.
+	            N2 = 1.
+
+            N = N1
+            if N==nbClust:
+
+	            # interpolation 
+	            t1 = []
+	            for point in points1:
+		            tmp = point/spacing1
+		            #print tmp, shape
+		            t1.append( dt1[int(tmp[0]),int(tmp[1]),int(tmp[2])] )
+	            t1 = np.asarray(t1).T
+	            t2 = []
+	            for point in points2:
+		            tmp = point/spacing2
+		            t2.append( dt2[int(tmp[0]),int(tmp[1]),int(tmp[2])] )
+	            t2 = np.asarray(t2).T
+
+	            # compute parameters
+	            m1 = np.mean(t1,1)
+	            m2 = np.mean(t2,1)
+	            d1 = t1 - np.reshape( np.repeat(m1,N1),(6,N1) )
+	            d2 = t2 - np.reshape( np.repeat(m2,N2),(6,N2) )
+	            s1 = np.sum( np.sum(d1[:3,:]**2,0) + 2*np.sum(d1[3:,:]**2,0),0 )
+	            s2 = np.sum( np.sum(d2[:3,:]**2,0) + 2*np.sum(d2[3:,:]**2,0),0 )
+	            s1 = np.sqrt( s1/(6*(N1-1)) )
+	            s2 = np.sqrt( s2/(6*(N2-1)) )
+
+	            # statistic
+	            m1_33 = np.array([[m1[0],m1[3],m1[4]],[m1[5],m1[1],m1[3]],[m1[4],m1[5],m1[2]]])
+	            m2_33 = np.array([[m2[0],m2[3],m2[4]],[m2[5],m2[1],m2[3]],[m2[4],m2[5],m2[2]]])
+	            L1,V1 = np.linalg.eigh(m1_33)
+	            L2,V2 = np.linalg.eigh(m2_33)
+	            stat = np.sum( (L1 - L2)**2 )
+	            s = 0.5 * ( s1**2+s2**2 )
+	            if s!=0 and (N-1)>0:
+		            stat = ((N-1.)*stat)/(6.*s)
+	            else:
+		            stat = 0.
+            else:
+	            stat = 0.
+
+            # normalize stat
+            df1 = 3.
+            df2 = 12.*(N-1.)
+
+            T.append( (stat,df1,df2) )
+
+        # impossible to get neighborhood
+        for cnt1 in range(shift_plane):
+            T.append( (0.,3.,1.) )
+
+        spm.append(np.asarray(T))
+
+    return spm
+
+
+
+def compute_statistic_bundle(dt1,dt2,C_hits_1,C_hits_2,d_thr=4.):
+    ''' Interpolate tensors (NN,L,...) and consider a paired statistic on bundles
+    '''
+
+    spm = []
+    dim1 = dt1.shape
+    dim2 = dt2.shape
+    spacing1 = dt1.spacing
+    spacing2 = dt2.spacing
+
+    for c1,c2 in zip(C_hits_1.keys(),C_hits_2.keys()):
+        sys.stdout.write("\riteration= %d, %d / %d, %d" %(c1,c2,len(C_hits_1.keys()),len(C_hits_2.keys())))
+        sys.stdout.flush()
+
+        # ref1 used to discard registration errors
+        ref1 = C_hits_1[c1]['skel']
+        population1 = C_hits_1[c1]['indices']
+        population2 = C_hits_2[c2]['indices']
+        T = []
+
+        if len(population1)==len(population2):
+            N = len(population1)
+        else:
+            raise medipy.base.Exception("Can only compare two populations of the same size")   
+
+        # density threshold
+        density = 0.
+        for pnts in population2:
+            density+=len(pnts)/N
+
+        # cluster & interpolation
+        skel1 = []
+        skel2 = []
+        if density>d_thr:
+            kk = True
+        else:
+            kk = False
+
+        if kk:
+            for cnt in range(N):
+	            pnts1 = population1[cnt]
+	            pnts2 = population2[cnt]
+	            pntref = np.cast[int](np.round(ref1[cnt]/spacing1))     
+	            pop1_tmp = []
+	            pop2_tmp = []
+	            if not (pntref<dim).all():
+		            kk = False
+		            break
+	            for point in pnts1:
+		            if (point<dim).all():
+			            pop1_tmp.append( dt1[point[0],point[1],point[2]] )
+	            for point in pnts2:
+		            if (point<dim).all():
+			            pop2_tmp.append( dt2[point[0],point[1],point[2]] )
+
+	            pop1_tmp = np.asarray(pop1_tmp).T
+	            pop2_tmp = np.asarray(pop2_tmp).T
+	            skel1.append( np.mean(pop1_tmp,1) )
+	            skel2.append( np.mean(pop2_tmp,1) )
+
+        if kk:
+            t1 = np.asarray(skel1).T
+            t2 = np.asarray(skel2).T
+            t1 = keep_eigenmatrice(t1)
+            t2 = keep_eigenmatrice(t2)
+
+            # compute parameters
+            diff = t2-t1
+            m = np.mean(diff,1)
+            d = diff - np.reshape( np.repeat(m,N),(6,N) )
+            s = np.sum( np.sum(d[:3,:]**2,0) + 2*np.sum(d[3:,:]**2,0),0 )
+            s = np.sqrt( s/(6.*(float(N)-1.)) )
+
+            # statistic
+            m_33 = np.array([[m[0],m[3],m[4]],[m[5],m[1],m[3]],[m[4],m[5],m[2]]])
+            L1,V1 = np.linalg.eigh(m_33)
+            L2 = np.array([0.,0.,0.])
+            stat = np.sum( (L1 - L2)**2 )
+            s = s**2
+            if s!=0 and (N-1)>0:
+	            stat = ((float(N)-1.)*stat)/(3*s)
+            else:
+	            stat = 0.
+        else:
+            stat = 0.
+
+        df1 = 3.
+        df2 = 6.*(N-1.)
+        for cnt in range(N):
+            T.append( (stat,df1,df2) )
+
+        spm.append(np.asarray(T))
+
+    return spm
+
+
+def keep_eigenmatrice(t):
+    n1,n2 = t.shape
+    res = []
+    anti = np.zeros((3,))
+    for cnt in range(n2):
+        m = t[:,cnt]
+        m_33 = np.array([[m[0],m[3],m[4]],[m[5],m[1],m[3]],[m[4],m[5],m[2]]])
+        L,V = np.linalg.eigh(m_33)
+        L = np.concatenate((L,anti))
+        res.append( L )
+    return np.asarray(res).T
+
+
+
+def project_fiber_stat_nn(spm,C_hits,model):
+
+    P = np.ones(model.shape,dtype=np.single)*(-1)
+    dim = model.shape
+
+    for c,t in zip(C_hits.keys(),spm):
+        sys.stdout.write("\riteration= %d / %d" %(c,len(C_hits.keys())))
+        sys.stdout.flush()
+
+        population = C_hits[c]['indices']
+        if t.ndim==1:
+            T = t[:]
+        else:
+            T = t[:,0]
+
+        for nb,points in enumerate(population):        
+            for p in points:
+                z,y,x = p
+                if x>=0 and y>=0 and z>=0 and x<dim[2] and y<dim[1] and z<dim[0]:
+                    cv = P[z,y,x]
+                    nv = T[nb]
+                    if nv>cv:
+                        P[z,y,x] = nv
+
+    return medipy.base.Image(data=P,spacing=model.spacing,origin=model.origin,direction=model.direction)
+
+
 
 
 def bundle_cross_sections_voxel(hits,spacing,cutoff,pv_thr) :
@@ -56,6 +330,35 @@ def cluster_voxel(data,spacing):
     list2 = [ list(y) for y in set([tuple(x) for x in list1]) ]
     list2 = [ np.asarray(x) for x in list2 ]
     return np.asarray(list2)
+
+def cluster_voxel_2(data,center,weights,spacing):
+
+    # sort by plane
+    argmax = np.argsort(weights)
+    weights = np.cast[int](weights[argmax])
+    data = data[argmax]
+    keys = np.unique(weights)
+    points = np.zeros((0,3))
+    for key in keys:
+        idx = np.where(weights==key)
+        tmp = data[idx]*spacing
+        d = np.sqrt( np.sum( (tmp-center)**2,1 ) )
+        argmax = np.argsort(d)
+        tmp = tmp[argmax]
+        points = np.concatenate( (points,tmp) )
+
+    points = np.cast[int]( np.round(points/spacing) )
+
+    list1 = list(points)
+    list2 = [ list(y) for y in set([tuple(x) for x in list1]) ]
+    list2 = [ np.asarray(x) for x in list2 ]
+    list2 = np.asarray(list2)*spacing
+
+    d = np.sqrt( np.sum( (list2-center)**2,1 ) )
+    argmax = np.argsort(d)
+    list2 = list2[argmax]
+
+    return list2
 
 
 
