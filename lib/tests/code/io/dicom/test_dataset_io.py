3df57bb96250b24c565b03a5d6503f454df532a5
Julien Lamy <lamy@unistra.fr>
2012-12-03 11:16:38 +0100

    lib/io/dicom : update tests and doc for "write"

--- lib/tests/code/io/dicom/test_dataset_io.py
+++ lib/tests/code/io/dicom/test_dataset_io.py
@@ -1,9 +1,11 @@
 import os
+import shutil
+import tempfile
 import unittest
 
 import medipy.io.dicom
 
-class testParse(unittest.TestCase):
+class testDataSetIO(unittest.TestCase):
             
     def setUp(self):
         data_directory = os.path.abspath(os.path.join(
@@ -30,6 +32,34 @@ class testParse(unittest.TestCase):
         self.assertEqual(
              self.dataset.referenced_image_sequence[0].referenced_sop_instance_uid,
              "1.3.12.2.1107.5.2.32.35389.201007230834425475364627")
+    
+    def test_write(self):
+        
+        tempdir = tempfile.mkdtemp()
+        
+        medipy.io.dicom.write(self.dataset, os.path.join(tempdir, "foo.dcm"))
+        other_dataset = medipy.io.dicom.read(os.path.join(tempdir, "foo.dcm"))
+        
+        shutil.rmtree(tempdir)
+        
+        # Check that other_dataset is included in self.dataset
+        for tag in sorted(self.dataset) :
+            if tag == (0x0008,0x0005) :
+                # Specific Character Set is not tested, since file might be re-encoded
+                continue
+            elif tag.private :
+                # TODO : private tags
+                continue
+            
+            self.assertTrue(tag in other_dataset)
+            self.assertEqual(self.dataset[tag], other_dataset[tag])
+        # Check that dataset is included in other_dataset
+        for tag in sorted(other_dataset) :
+            if tag.private :
+                # TODO : private tags
+                continue
+            self.assertTrue(tag in self.dataset)
+            # No need to check for equality, we just did it.
 
 if __name__ == '__main__':
     unittest.main()
