7eb8e92baeeb60b06a138690e80ff98a8d9a1d73
Julien Lamy <lamy@unistra.fr>
2012-12-13 11:47:28 +0100

    lib/io/dicom/dictionary : update to 2011 standard

--- lib/medipy/io/dicom/generate_public_dictionary.py
+++ lib/medipy/io/dicom/generate_public_dictionary.py
@@ -1,9 +1,9 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011             
-# Distributed under the terms of the CeCILL-B license, as published by 
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
-# for details.                                                      
+# MediPy - Copyright (C) Universite de Strasbourg
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
 ##########################################################################
 
 import sys
@@ -30,21 +30,22 @@ def main():
     
     data_dictionary = {}
     
-    for child in input.getElementsByTagName("dict")[0].childNodes :
-        if child.nodeType != xml.dom.Node.ELEMENT_NODE or child.nodeName != "entry":
-            continue
+    for dictionary in input.getElementsByTagName("dict") :
+        for entry in dictionary.childNodes :
+            if entry.nodeName != "entry" :
+                continue
         
-        tag = child.getAttribute("group") + child.getAttribute("element")
-        
-        if "x" not in tag :
-            tag = int(tag, 16)
-        
-        vr = "/".join((child.getAttribute("vr") or "UN").split("_"))
-        vm = child.getAttribute("vm") or "1"
-        name = child.getAttribute("name")
-        retired = (child.getAttribute("retired") == "true")
-        
-        data_dictionary[tag] = (vr, vm, name, retired, underscored_name(name))
+            tag = entry.getAttribute("group") + entry.getAttribute("element")
+            
+            if "x" not in tag :
+                tag = int(tag, 16)
+            
+            vr = "/".join((entry.getAttribute("vr") or "UN").split("_"))
+            vm = entry.getAttribute("vm") or "1"
+            name = entry.getAttribute("name")
+            retired = (entry.getAttribute("retired") == "true")
+            
+            data_dictionary[tag] = (vr, vm, name, retired, underscored_name(name))
     
     print "data_dictionary = {"
     
