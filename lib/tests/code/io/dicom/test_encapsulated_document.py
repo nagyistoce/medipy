42c9b613e9c183bff5f7528338866a055181752c
Julien Lamy <lamy@unistra.fr>
2012-12-14 18:11:13 +0100

    lib/io/dicom : add encapsulated_document

---
+++ lib/tests/code/io/dicom/test_encapsulated_document.py
@@ -0,0 +1,111 @@
+import filecmp
+import os
+import shutil
+import tempfile
+import unittest
+
+import numpy
+
+import medipy.io.dicom
+
+class TestEncapsulatedDocument(unittest.TestCase):
+    
+    def generate_file(self, destination, size):
+        # Random return float, we don't need as many bytes
+        itemsize = numpy.dtype(float).itemsize
+        data = numpy.random.random((1+size/itemsize)).tostring()[:size]
+        
+        fd = open(destination, "wb")
+        fd.write(data)
+        fd.close()
+    
+    def test_encapsulate_even_size(self):
+        temporary_directory = tempfile.mkdtemp()
+        
+        input = os.path.join(temporary_directory, "input")
+        self.generate_file(input, 40000)
+        
+        dataset = medipy.io.dicom.encapsulated_document.encapsulate(input, "foo/bar")
+        self._test_encapsulate(dataset, "foo/bar", "EVEN")
+        
+        shutil.rmtree(temporary_directory)
+    
+    def test_encapsulate_odd_size(self):
+        temporary_directory = tempfile.mkdtemp()
+        
+        input = os.path.join(temporary_directory, "input")
+        self.generate_file(input, 40001)
+        
+        dataset = medipy.io.dicom.encapsulated_document.encapsulate(input, "foo/bar")
+        self._test_encapsulate(dataset, "foo/bar", "ODD")
+    
+        shutil.rmtree(temporary_directory)
+    
+    def test_expose_even_size(self):
+        temporary_directory = tempfile.mkdtemp()
+        
+        input = os.path.join(temporary_directory, "input")
+        output = os.path.join(temporary_directory, "output")
+        self.generate_file(input, 40000)
+        
+        dataset = medipy.io.dicom.encapsulated_document.encapsulate(input)
+        medipy.io.dicom.encapsulated_document.expose(dataset, output)
+        
+        self.assertTrue(filecmp.cmp(input, output, False))
+    
+        shutil.rmtree(temporary_directory)
+    
+    def test_expose_odd_size(self):
+        temporary_directory = tempfile.mkdtemp()
+        
+        input = os.path.join(temporary_directory, "input")
+        output = os.path.join(temporary_directory, "output")
+        self.generate_file(input, 40001)
+        
+        dataset = medipy.io.dicom.encapsulated_document.encapsulate(input)
+        medipy.io.dicom.encapsulated_document.expose(dataset, output)
+        
+        self.assertTrue(filecmp.cmp(input, output, False))
+    
+        shutil.rmtree(temporary_directory)
+
+    def _test_encapsulate(self, dataset, mime_type, parity):
+        # MIME type
+        self.assertTrue("mime_type_of_encapsulated_document" in dataset)
+        mime_type_of_encapsulated_document = dataset.mime_type_of_encapsulated_document
+        self.assertTrue(isinstance(mime_type_of_encapsulated_document, medipy.io.dicom.LO))
+        self.assertTrue(isinstance(mime_type_of_encapsulated_document.value, str))
+        self.assertEqual(mime_type_of_encapsulated_document, mime_type)
+        
+        # File length parity
+        self.assertTrue("document_class_code_sequence" in dataset)
+        document_class_code_sequence = dataset.document_class_code_sequence
+        self.assertTrue(isinstance(document_class_code_sequence, medipy.io.dicom.SQ))
+        self.assertTrue(isinstance(document_class_code_sequence.value, list))
+        self.assertEqual(len(document_class_code_sequence), 1)
+        
+        code = document_class_code_sequence[0]
+        self.assertTrue(isinstance(code, medipy.io.dicom.DataSet))
+        
+        self.assertTrue("code_value" in code)
+        self.assertTrue(isinstance(code.code_value, medipy.io.dicom.SH))
+        self.assertTrue(isinstance(code.code_value.value, str))
+        self.assertEqual(code.code_value.value, "LENGTH_PARITY:{0}".format(parity))
+        
+        self.assertTrue("coding_scheme_designator" in code)
+        self.assertTrue(isinstance(code.coding_scheme_designator, medipy.io.dicom.SH))
+        self.assertTrue(isinstance(code.coding_scheme_designator.value, str))
+        self.assertEqual(code.coding_scheme_designator.value, "MEDIPY")
+        
+        self.assertTrue("code_meaning" in code)
+        self.assertTrue(isinstance(code.code_meaning, medipy.io.dicom.LO))
+        self.assertTrue(isinstance(code.code_meaning.value, str))
+        self.assertEqual(code.code_meaning.value, "LENGTH_PARITY:{0}".format(parity))
+        
+        # Encapsulated document
+        self.assertTrue("encapsulated_document" in dataset)
+        self.assertTrue(isinstance(dataset.encapsulated_document, medipy.io.dicom.OB))
+        self.assertTrue(isinstance(dataset.encapsulated_document.value, str))
+    
+if __name__ == "__main__" :
+    unittest.main()
