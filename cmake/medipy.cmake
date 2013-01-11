7c5b35010765a40feaca0402429586fd260d4567
Julien Lamy <lamy@unistra.fr>
2013-01-11 16:18:38 +0100

    cmake/medipy : enable OpenMP if detected

--- cmake/medipy.cmake
+++ cmake/medipy.cmake
@@ -1,3 +1,10 @@
 include("cmake/itk.cmake")
 include("cmake/python.cmake")
 include("cmake/vtk.cmake")
+
+find_package(OpenMP)
+if(OPENMP_FOUND)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+endif()
