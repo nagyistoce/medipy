7d273c66fcf742c67be1e9efc97671af02f712ac
Julien Lamy <lamy@unistra.fr>
2013-01-14 16:28:36 +0100

    lib/network/dicom : update documentation

--- lib/medipy/network/dicom/scu/SCU.py
+++ lib/medipy/network/dicom/scu/SCU.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2012
+# MediPy - Copyright (C) Universite de Strasbourg
 # Distributed under the terms of the CeCILL-B license, as published by
 # the CEA-CNRS-INRIA. Refer to the LICENSE file or to
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -9,7 +9,7 @@
 class SCU(object) :
     """ Base class for all DICOM Service Class Users. 
     
-        All derived classes must implement the __call__ function, which performs
+        All derived classes must implement the ``__call__`` function, which performs
         the actual operation.
     """
     
