e1186ba8457a324d043824c8374894ea1fe6724e
Julien Lamy <lamy@unistra.fr>
2013-01-14 17:35:55 +0100

    plugins/torque/qsub : return the job id

--- plugins/torque/qsub.py
+++ plugins/torque/qsub.py
@@ -6,7 +6,8 @@ import subprocess
 import arguments
 
 def submit(job_script, script_arguments, working_directory):
-    """ Submit a single job to a Torque master node.
+    """ Submit a single job to a Torque master node. The job identifier is 
+        returned, unless an error occured. In that case, an exception is raised.
     """
     
     # Copy the job script to the working directory
@@ -15,10 +16,19 @@ def submit(job_script, script_arguments, working_directory):
     
     # Run qsub from the working directory
     environment = "medipy_torque={0}".format(arguments.encode(script_arguments))
-    subprocess.call(["qsub", remote_job_script, "-v", environment], cwd=working_directory)
+    command = ["qsub", remote_job_script, "-v", environment]
+    process = subprocess.Popen(command, cwd=working_directory, 
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode == 0 :
+        job_id = stdout.strip()
+        return job_id
+    else :
+        raise medipy.base.Exception(stderr.strip())
 
 def submit_array(job_script, scripts_arguments_list, working_directory):
-    """ Submit an array of jobs to a Torque master node.
+    """ Submit an array of jobs to a Torque master node. The job identifier is 
+        returned, unless an error occured. In that case, an exception is raised.
     
         scripts_arguments_list must be a list of arguments, each item will be
         passed to one of the individual jobs.
@@ -30,9 +40,13 @@ def submit_array(job_script, scripts_arguments_list, working_directory):
     
     # Run qsub from the working directory
     environment = "medipy_torque={0}".format(arguments.encode(scripts_arguments_list))
-    subprocess.call([
-         "qsub", 
-         "-t", "0-{0}".format(len(scripts_arguments_list)-1),
-         "-v", environment, 
-         remote_job_script
-    ], cwd=working_directory)
+    command = ["qsub", "-t", "0-{0}".format(len(scripts_arguments_list)-1),
+               "-v", environment, remote_job_script]
+    process = subprocess.Popen(command, cwd=working_directory, 
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode == 0 :
+        job_id = stdout.strip()
+        return job_id
+    else :
+        raise medipy.base.Exception(stderr.strip())
