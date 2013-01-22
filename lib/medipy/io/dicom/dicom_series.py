c3bab93f9ae0e8c0be846065e50fce20b038d3bf
Julien Lamy <lamy@unistra.fr>
2013-01-22 11:03:38 +0100

    lib/io/dicom/DicomSeries : add custom_name_from_uid

--- lib/medipy/io/dicom/dicom_series.py
+++ lib/medipy/io/dicom/dicom_series.py
@@ -33,6 +33,17 @@ class DicomSeries(object) :
         matches = [x for x in self.series if x[0] == uid]
         return (len(matches) != 0)
 
+    def custom_name_from_uid(self, uid) :
+        """ Return the custom name associated with the Series Instance UID
+        """
+        
+        if not self.has_uid(uid) :
+            raise medipy.base.Exception("No such UID {0}".format(repr(uid)))
+        matches = [x for x in self.series if x[0] == uid]
+        if len(matches) > 1 :
+            logging.warning("UID is not unique : {0}".format(uid))
+        return matches[0][2]
+
     def url_from_uid(self, uid) :
         """ Return the URL associated with the Series Instance UID
         """
@@ -80,8 +91,33 @@ class DicomSeries(object) :
             logging.warning("Custom name is not unique : {0}".format(custom_name))
         return "{0}#series_instance_uid={1}".format(self.root, matches[0][0])
 
+    def write(self, filename):
+        """ Write the dictionary to a file.
+        
+            The dictionary is saved in CSV, using the following format :
+            
+            * The first line contains only the root URL
+            * Each successive line contain the UID, description and custom
+              name of each series.
+        """
+        
+        fd = open(filename, "w")
+        
+        fd.write("{0}\n".format(self.root))
+        # TODO : delimiter & quoting
+        writer = csv.writer(fd)
+        for uid, description, custom_name in self.series :
+            writer.writerow(
+                [uid, description.encode("ascii", "replace"), custom_name])
+        fd.close()
+
     @staticmethod
     def read(filename) :
+        """ Read a dictionary from a file.
+            
+            See :func:`write` for the file format.
+        """
+        
         fd = open(filename)
         root = fd.readline().strip()
         reader = csv.reader(fd)
