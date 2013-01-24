e7e4b72b10c2ae15f0cba132266c9a9e79ec9178
Julien Lamy <lamy@unistra.fr>
2013-01-24 12:06:53 +0100

    lib/gui/annotations : update doc

---
+++ doc/fr/reference/lib/medipy/gui/index.rst
@@ -0,0 +1,13 @@
+:mod:`medipy.gui` --- Graphical user interface 
+==============================================
+
+The following sub-modules are also documented :
+
+.. toctree::
+    
+    annotations
+    shapes
+
+----
+
+.. automodule:: medipy.gui
--- lib/medipy/gui/annotations/annotations_panel.py
+++ lib/medipy/gui/annotations/annotations_panel.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011-2012
+# MediPy - Copyright (C) Universite de Strasbourg
 # Distributed under the terms of the CeCILL-B license, as published by
 # the CEA-CNRS-INRIA. Refer to the LICENSE file or to
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -25,6 +25,8 @@ import medipy.gui.xrc_wrapper
 from medipy.gui.control import Float 
 
 class AnnotationsPanel(medipy.gui.base.Panel):
+    """ GUI showing a list of annotations.
+    """
     
     class UI(medipy.gui.base.UI):
         def __init__(self):
