61e44ab175e8e2649df2188b586780acdced7203
Julien Lamy <lamy@unistra.fr>
2013-01-28 09:29:57 +0100

    lib/vtk/vtkColorTransferFunctionWithAlpha : fix AddRGBAPoint

--- lib/medipy/vtk/vtkColorTransferFunctionWithAlpha.cxx
+++ lib/medipy/vtk/vtkColorTransferFunctionWithAlpha.cxx
@@ -1,5 +1,5 @@
 /*************************************************************************
- * MediPy - Copyright (C) Universite de Strasbourg, 2011
+ * MediPy - Copyright (C) Universite de Strasbourg
  * Distributed under the terms of the CeCILL-B license, as published by
  * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
  * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -60,7 +60,7 @@ vtkColorTransferFunctionWithAlpha
 
     // Find the index of the first point in Function with value greater of equal
     // to x
-    for(i=0; i<this->NumberOfPoints; i++, fptr += 4)
+    for(i=0; i<this->NumberOfPoints; i++, fptr += 5)
     {
         if(x <= *fptr)
         {
