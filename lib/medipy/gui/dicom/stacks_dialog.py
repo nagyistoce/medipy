68d3208234c3fef9b88bebbe07f769002ec2c809
Julien Lamy <lamy@unistra.fr>
2013-02-04 11:57:46 +0100

    lib/gui/dicom/stacks_dialog : fix w.r.t. using medipy.io.dicom.VR

--- lib/medipy/gui/dicom/stacks_dialog.py
+++ lib/medipy/gui/dicom/stacks_dialog.py
@@ -1,15 +1,17 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011             
-# Distributed under the terms of the CeCILL-B license, as published by 
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
-# for details.                                                      
+# MediPy - Copyright (C) Universite de Strasbourg
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
 ##########################################################################
 
 import sys
 
 import numpy
 import wx
+
+import medipy.io.dicom
 
 class StacksDialog(wx.Dialog):
     """ Display a dialog for all stacks of a DICOM series (i.e. sets of images
@@ -80,7 +82,8 @@ class StacksDialog(wx.Dialog):
         self._listctrl.InsertColumn(sys.maxint, "Number of images")
         
         for stack in self._stacks :
-            value = self._format_image_orientation_patient(stack[0].image_orientation_patient)
+            orientation = stack[0].image_orientation_patient.value
+            value = self._format_image_orientation_patient(orientation)
             item = self._listctrl.InsertStringItem(sys.maxint, value)
             self._listctrl.SetStringItem(item, 1, "{0}".format(len(stack)))
             self._item_data[item] = stack
