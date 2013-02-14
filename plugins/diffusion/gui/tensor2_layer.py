e64b8ef47cfd6634c32c50bc70e6ec622e98389b
Antoine Grigis <grigis@unistra.fr>
2013-02-14 11:23:51 +0100

    plugins/diffusion/gui/tensor2_layer : Fix the visualization of ellipsoids

--- plugins/diffusion/gui/tensor2_layer.py
+++ plugins/diffusion/gui/tensor2_layer.py
@@ -243,7 +243,7 @@ class Tensor2Layer(medipy.gui.image.Layer) :
             self._change_information.GetOutput(), None)
         if self.display_coordinates in ["physical", "nearest_axis_aligned"] :
             
-            matrix = self.image.direction
+            matrix = numpy.dot(self.world_to_slice, numpy_slice_tensors.direction)
             if self.display_coordinates == "nearest_axis_aligned" :
                 matrix = medipy.base.coordinate_system.\
                             best_fitting_axes_aligned_matrix(matrix)
