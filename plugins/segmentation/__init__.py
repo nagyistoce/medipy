e27634b68a45eb41242b5c2824479a8ae6f361e1
Julien Lamy <lamy@unistra.fr>
2013-03-08 11:47:25 +0100

    plugins/segmentation : store all ITK modules in MediPySegmentation

--- plugins/segmentation/__init__.py
+++ plugins/segmentation/__init__.py
@@ -1,7 +1,6 @@
 import os.path
 import medipy.itk
 
-medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "BETImageFilter")
-medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "StatisticalChangeDetectionImageFilters")
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MediPySegmentation")
 
 from api import *
