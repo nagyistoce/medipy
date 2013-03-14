ee5b6c0c83033b441cb938e24fc9af5e42b8ffe5
Julien Lamy <lamy@unistra.fr>
2013-03-14 09:36:48 +0100

    lib/itk/loader : adjust with respect to WrapITK/CMake file locations

--- lib/medipy/itk/loader.py
+++ lib/medipy/itk/loader.py
@@ -1,12 +1,12 @@
 ##########################################################################
-# MediPy - Copyright (C) Universite de Strasbourg, 2011             
-# Distributed under the terms of the CeCILL-B license, as published by 
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to            
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html       
-# for details.                                                      
+# MediPy - Copyright (C) Universite de Strasbourg
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
 ##########################################################################
 
-import imp
+import os
 import sys
 
 import itk
@@ -14,12 +14,17 @@ import itkBase
 
 def load_wrapitk_module(path, name):
     """ Load a WrapITK module, and add its symbols to the itk module """
+
+    old_path = sys.path
     
-    sys.path.append(path)
+    sys.path = sys.path+[path]
     
     config_module_name = "{0}Config".format(name)
-    file, pathname, description = imp.find_module(config_module_name, [path])
-    config_module = imp.load_module(config_module_name, file, pathname, description)
+    config_module = __import__(config_module_name)
+    
+    path = os.path.dirname(os.path.abspath(config_module.__file__))
+    if path not in sys.path :
+        sys.path = sys.path + [path]
     
     data = config_module.__dict__
     itkBase.module_data[name] = data
@@ -30,3 +35,5 @@ def load_wrapitk_module(path, name):
     for k, v in namespace.items():
         if not hasattr(itk, k) :
             setattr(itk, k, v)
+    
+    sys.path = old_path
