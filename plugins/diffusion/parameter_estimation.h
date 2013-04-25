65d60378d152d4a6a29d06754df1bf4ae870d676
Julien Lamy <lamy@unistra.fr>
2013-04-25 13:45:28 +0200

    plugins/diffusion : add missing license headers

--- plugins/diffusion/parameter_estimation.h
+++ plugins/diffusion/parameter_estimation.h
@@ -1,4 +1,10 @@
-
+/*************************************************************************
+ * MediPy - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
 #ifndef dtiParam_itk_h
 #define dtiParam_itk_h
 
