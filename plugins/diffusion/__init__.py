dbd9dc32a1047ccb9a3d2e31842539c91e47c99e
Julien Lamy <lamy@unistra.fr>
2013-05-14 15:31:36 +0200

    plugins/diffusion/tractography : fix seeding bug, clean up code

--- plugins/diffusion/__init__.py
+++ plugins/diffusion/__init__.py
@@ -6,7 +6,6 @@ medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MediPyDiffusion")
 import estimation
 import gui
 import io
-from spectral_analysis import spectral_analysis
 import scalars
 import tractography
 import utils
