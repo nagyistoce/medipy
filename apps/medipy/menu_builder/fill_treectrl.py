320c1327ab4637b574b75efe618897e64151f9a0
Antoine Grigis <grigis@unistra.fr>
2013-05-16 14:59:43 +0200

    apps/medipy: search option in fct menu treectrl

--- apps/medipy/menu_builder/fill_treectrl.py
+++ apps/medipy/menu_builder/fill_treectrl.py
@@ -6,16 +6,23 @@
 # for details.                                                      
 ##########################################################################
 
-def fill_treectrl(menu, treectrl):
+def fill_treectrl(menu, treectrl, match):
     """ Fill a treectrl with the menu. Each menu item is a triplet 
         (label, function, children). If no function is associated with this item
         it is set to None.
     """
     root = treectrl.AddRoot("Functions")
-    add_tree_nodes(root, menu, treectrl)
+    add_tree_nodes(root, menu, treectrl,match)
 
-def add_tree_nodes(parentItem, items, treectrl):
-    for label, function, children in items:
-        tree_item = treectrl.AppendItem(parentItem, label)
-        treectrl.SetPyData(tree_item, function)
-        add_tree_nodes(tree_item, children, treectrl)
+def add_tree_nodes(parentItem, items, treectrl,match):
+    for label,function,children in items:
+        if match in label.lower() or search_in_menu(children,match) or match=="" :
+            tree_item = treectrl.AppendItem(parentItem, label)
+            treectrl.SetPyData(tree_item, function)
+            add_tree_nodes(tree_item, children, treectrl,match)
+
+def search_in_menu(items,match) :
+    key = False
+    for label,function,children in items:
+        key = key or ( match in label.lower() or any( search_in_menu([child],match) for child in children ) )
+    return key
