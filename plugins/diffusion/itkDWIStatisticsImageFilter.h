13ddf3600412879c7e296097d20d188393560a03
Julien Lamy <lamy@unistra.fr>
2013-05-16 15:00:31 +0200

    plugins/diffusion/itkDWIStatisticsImageFilter : add missing license header

--- plugins/diffusion/itkDWIStatisticsImageFilter.h
+++ plugins/diffusion/itkDWIStatisticsImageFilter.h
@@ -1,3 +1,11 @@
+/*************************************************************************
+ * MediPy - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
 #ifndef _2dd8b74d_fbaf_4ee0_ac0b_e4e910c4f229
 #define _2dd8b74d_fbaf_4ee0_ac0b_e4e910c4f229
 
