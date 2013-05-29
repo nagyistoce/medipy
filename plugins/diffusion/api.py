b8307687fbe5585d4e0aa72714892aea9409dfca
Julien Lamy <lamy@unistra.fr>
2013-05-29 14:42:34 +0200

    plugins/diffusion/statistic : fix gui

--- plugins/diffusion/api.py
+++ plugins/diffusion/api.py
@@ -2,3 +2,5 @@ from estimation import least_squares as least_squares_estimation
 from tractography import skeleton_gui as skeleton
 from tractography import streamline_gui as streamline_tractography
 from scalars import fractional_anisotropy, mean_diffusivity
+from statistic import (
+    bootstrap_parameter_estimation, spatial_parameter_estimation)
