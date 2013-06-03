02c4c2a37e5cc2ab7557f6c92ae55db10b0f7538
Julien Lamy <lamy@unistra.fr>
2013-06-03 14:26:42 +0200

    cmake : fix cython modules on Ubuntu 10.04

--- cmake/cython.cmake
+++ cmake/cython.cmake
@@ -4,9 +4,12 @@ if(CYTHON_FOUND)
     
     function(medipy_cython_add_module _name )
         cython_add_module(${_name} ${ARGN})
-        # Install the module in the same directory as its source file
-        set_target_properties(${_name} 
-            PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+        # Install the module in the same directory as its source file, and make
+        # sure no "lib" prefix is added to the target file.
+        set_target_properties(${_name} PROPERTIES 
+            LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            PREFIX ""
+        )
     endfunction()
 
 endif()
