36eaa3b83b015b491dfc8132f63a08947af9bcdb
Julien Lamy <lamy@unistra.fr>
2013-06-06 16:19:33 +0200

    plugins/diffusion/registration : fix GUI of apply_tensor_trf ; add to api and __init__

--- plugins/diffusion/api.py
+++ plugins/diffusion/api.py
@@ -1,4 +1,5 @@
 from estimation import least_squares as least_squares_estimation
+from registration import apply_tensor_trf
 from tractography import skeleton_gui as skeleton
 from tractography import streamline_gui as streamline_tractography
 from scalars import fractional_anisotropy, mean_diffusivity
