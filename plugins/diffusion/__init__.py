21d93726d0d7080a0e6fefd968befb0b0a0c9f00
Julien Lamy <lamy@unistra.fr>
2013-06-06 16:37:09 +0200

    plugins/diffusion : rename statistic to statistics

--- plugins/diffusion/__init__.py
+++ plugins/diffusion/__init__.py
@@ -4,9 +4,11 @@ import medipy.itk
 medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MediPyDiffusion")
 
 import estimation
+import fiber_statistics
 import gui
 import io
 import registration
 import scalars
+import statistics
 import tractography
 import utils
