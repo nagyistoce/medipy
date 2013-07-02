e4f878a0b4d3395327d3831788fc7a6ebcfc1ef8
Julien Lamy <lamy@unistra.fr>
2013-07-02 16:14:43 +0200

    lib/gui/network/dicom/query_dialog : fix recall of last connection when not in preferences

--- lib/medipy/gui/network/dicom/preferences_dialog.py
+++ lib/medipy/gui/network/dicom/preferences_dialog.py
@@ -6,10 +6,11 @@
 # for details.
 ##########################################################################
 
+import os
+import socket
+
 import wx
 import wx.grid
-
-import os
 
 import medipy.network.dicom
 import medipy.gui.base
@@ -120,7 +121,8 @@ class PreferencesDialog(medipy.gui.base.Panel):
     def OnAdd(self,_):
         """ Add a new connection set with default parameters
         """
-        connection = medipy.network.dicom.Connection("----", 0, "----", "----")
+        connection = medipy.network.dicom.Connection(
+            "----", 0, socket.gethostname(), "----")
         self.list_connections.append(["----", connection, "wado", "----"])
 
     def OnDelete(self,_):
