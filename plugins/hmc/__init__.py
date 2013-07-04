9ef57b8a0f97cc691beba2bf30434982443b65bf
Julien Lamy <lamy@unistra.fr>
2013-07-04 10:20:12 +0200

    plugins/hmc : fix module import

--- plugins/hmc/__init__.py
+++ plugins/hmc/__init__.py
@@ -1,6 +1,4 @@
 import os.path
 import medipy.itk
 
-medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "HiddenMarkovChainFilter")
-
-from api import *
+medipy.itk.load_wrapitk_module(os.path.dirname(__file__), "MediPyHMC")
