0d0f64eb0330cc2e0faae58b09284aa1a78324af
Julien Lamy <lamy@unistra.fr>
2013-07-11 17:14:55 +0200

    plugins/diffusion/api : re-add registration

--- plugins/diffusion/api.py
+++ plugins/diffusion/api.py
@@ -1,5 +1,6 @@
 from estimation import least_squares as least_squares_estimation
 from estimation import weighted_least_squares as weighted_least_squares_estimation
+from registration import apply_tensor_trf
 from scalars import fractional_anisotropy, mean_diffusivity
 from statistics import (
     spatial_parameter_estimation, bootstrap_parameter_estimation, 
