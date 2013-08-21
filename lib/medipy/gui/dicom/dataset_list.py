79e26d9b612fd89ec0a738a3e0bc362846806abb
Julien Lamy <lamy@unistra.fr>
2013-08-21 17:24:00 +0200

    lib/gui/dicom/dataset_list: fix display of multiframe data

--- lib/medipy/gui/dicom/dataset_list.py
+++ lib/medipy/gui/dicom/dataset_list.py
@@ -57,7 +57,7 @@ class DataSetList(wx.ListCtrl) :
             item = self.InsertStringItem(sys.maxint, "Per-frame elements")
             self.SetItemBackgroundColour(item, (201,237,255))
             if "perframe_functional_groups_sequence" in dataset :
-                self._add_dataset(dataset.perframe_functional_groups_sequence[frame_number])
+                self._add_dataset(dataset.perframe_functional_groups_sequence.value[frame_number])
         
         for i in range(self.GetColumnCount()):
             self.SetColumnWidth(i, wx.LIST_AUTOSIZE)
