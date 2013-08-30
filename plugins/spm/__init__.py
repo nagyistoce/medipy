bbef8c6193ae7d62dc06c378bd5f3d77fa8672a0
Julien Lamy <lamy@unistra.fr>
2013-08-30 13:54:20 +0200

    plugins/spm : add deformations module

--- plugins/spm/__init__.py
+++ plugins/spm/__init__.py
@@ -2,10 +2,11 @@ import utils
 
 from base import script
 from contrast_manager import ContrastManager
+from deformations import Deformations
 from factorial_design import FactorialDesign
 from model_estimation import ModelEstimation
 from new_segment import NewSegment
 from result_report import ResultReport
 
-__all__ = ["script", "ContrastManager", "FactorialDesign", "ModelEstimation", 
-           "NewSegment", "ResultReport"]
+__all__ = ["script", "ContrastManager", "Deformations", "FactorialDesign", 
+           "ModelEstimation", "NewSegment", "ResultReport"]
