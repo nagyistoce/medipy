f148dea962653dbfc49eae378e2fea9e3a8e757f
Julien Lamy <lamy@unistra.fr>
2014-01-24 15:45:42 +0100

    plugins/diffusion/scalars: add axial diffusivity and radial diffusivity

--- plugins/diffusion/scalars.py
+++ plugins/diffusion/scalars.py
@@ -8,7 +8,19 @@
 
 import itk
 import medipy.itk
-import numpy as np
+import numpy
+
+def axial_diffusivity(image):
+    """ Compute the axial diffusivity from a second order symmetric tensor field.
+    
+        <gui>
+            <item name="image" type="Image" label="Input"/>
+            <item name="output" type="Image" initializer="output=True" 
+                  role="return" label="Output"/>
+        </gui>
+    """
+    
+    return _compute_scalar(image, "AxialDiffusivity")
 
 def fractional_anisotropy(image):
     """ Compute the fractional anisotropy from a second order symmetric tensor field.
@@ -20,18 +32,7 @@ def fractional_anisotropy(image):
         </gui>
     """
     
-    itk_image = medipy.itk.medipy_image_to_itk_image(image, False)
-    ScalarImage = itk.Image[itk.template(itk_image)[1]]
-    
-    fa_filter = itk.FractionalAnisotropyImageFilter[itk_image, ScalarImage].New()
-    fa_filter.SetInput(0,itk_image)
-    fa_filter.Update()
-    itk_output = fa_filter.GetOutput(0)
-    output = medipy.itk.itk_image_to_medipy_image(itk_output,None,True)
-    output.data[np.isnan(output.data)] = 0
-    output.data[np.isinf(output.data)] = 0
-
-    return output
+    return _compute_scalar(image, "FractionalAnisotropy")
 
 def mean_diffusivity(image):
     """ Compute the mean diffusivity from a second order symmetric tensor field.
@@ -43,13 +44,35 @@ def mean_diffusivity(image):
         </gui>
     """
     
+    return _compute_scalar(image, "MeanDiffusivity")
+
+def radial_diffusivity(image):
+    """ Compute the radial diffusivity from a second order symmetric tensor field.
+    
+        <gui>
+            <item name="image" type="Image" label="Input"/>
+            <item name="output" type="Image" initializer="output=True" 
+                  role="return" label="Output"/>
+        </gui>
+    """
+    
+    return _compute_scalar(image, "RadialDiffusivity")
+
+def _compute_scalar(image, scalar_name):
+    """ Compute a scalar from a tensor image.
+    """
+    
+    Filter = getattr(itk, "{0}ImageFilter".format(scalar_name))
+    
     itk_image = medipy.itk.medipy_image_to_itk_image(image, False)
     ScalarImage = itk.Image[itk.template(itk_image)[1]]
     
-    md_filter = itk.MeanDiffusivityImageFilter[itk_image, ScalarImage].New()
-    md_filter.SetInput(0,itk_image)
-    md_filter.Update()
-    itk_output = md_filter.GetOutput(0)
-    output = medipy.itk.itk_image_to_medipy_image(itk_output,None,True)
-
+    filter_ = Filter[itk_image, ScalarImage].New(Input=itk_image)
+    filter_()
+    itk_output = filter_[0]
+    
+    output = medipy.itk.itk_image_to_medipy_image(itk_output, None, True)
+    output.data[numpy.isnan(output.data)] = 0
+    output.data[numpy.isinf(output.data)] = 0
+    
     return output
