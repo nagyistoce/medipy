f6886586c5f885ba176554023db705446eac2603
Julien Lamy <lamy@unistra.fr>
2014-04-23 12:24:41 +0200

    lib/io/dicom/csa2: add parsing of protocol structure

--- lib/medipy/io/dicom/csa2.py
+++ lib/medipy/io/dicom/csa2.py
@@ -6,6 +6,7 @@
 # for details.
 ##########################################################################
 
+import re
 import struct
 
 def parse_csa(csa):
@@ -83,3 +84,81 @@ def parse_item(csa, start):
     content, padding = struct.unpack(format, csa[start+header_size:start+header_size+content_size])
     
     return content, header_size+content_size
+
+def parse_protocol(data):
+    """ Parse (as a nested dictionary) the ASCII version of protocol data.
+    """
+    
+    integer_types = [
+        "c", "i", "l", "n", "s",
+        "uc", "ui", "ul", "un", "us",
+    ]
+    floating_point_types = ["fl", "d"]
+    types = integer_types+floating_point_types+["s", "b", "t"]    
+    
+    def integer_parser(value):
+        return int(value, 16 if value.startswith("0x") else 10)
+    
+    def floating_point_parser(value):
+        return float(value)
+    
+    def string_parser(value):
+        # Remove beginning and ending quotes
+        if all(c=='"' for c in value):
+            return ""
+        else:
+            return re.match(r"\"*(.*[^\"])\"*", value).group(1)
+    
+    def value_parser(type_, value):
+        if type_ == "b":
+            value = bool(integer_parser(value))
+        elif type_ == "t":
+            value = string_parser(value)
+        elif type_ in integer_types:
+            value = integer_parser(value)
+        elif type_ in floating_point_types:
+            value = floating_point_parser(value)
+        else:
+            try:
+                value = integer_parser(value)
+            except ValueError:
+                try:
+                    value = floating_point_parser(value)
+                except ValueError:
+                    # Not a numerical value, do nothing
+                    pass
+        return value
+    
+    if isinstance(data, basestring):
+        data = data.splitlines()
+    
+    protocol = {}
+    for line in data:
+        match = re.match(r"^(?P<key>[\w\[\]\.]+)\s+=\s+(?P<value>.*)$", line)
+        key, value = match.groupdict()["key"], match.groupdict()["value"]
+        
+        entry = protocol
+        for element in key.split("."):
+            match = re.match(r"(a?)((?:{0})?)(\w+)(?:\[(\d+)\])?".format("|".join(types)), element)
+            is_array, type_, name, index = match.groups()
+            is_array = (is_array == "a")
+            if index:
+                index = int(index)
+            
+            if is_array:
+                entry = entry.setdefault(name, [])
+                
+                if len(entry) <= index:
+                    entry.extend((1+index-len(entry))*[None])
+                if type_ not in ("", "s"):
+                    entry[index] = value_parser(type_, value)
+                else:
+                    if entry[index] is None:
+                        entry[index] = {}
+                    entry = entry[index]
+            elif type_ == "s":
+                entry = entry.setdefault(name, {})
+            else:
+                entry.setdefault(name, value_parser(type_, value))
+    
+    return protocol
